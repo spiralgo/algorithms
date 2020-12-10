e654dabdce3a3065e2938388ae4ce314e5c3dd43
altayhunoglu <muratfair@gmail.com>
2020-12-10 23:44:31 +0300

    Burada hatırlamak gereken şu: "An extra node as a reference to the tail."
      curr = result;
      ...
      curr.next = new ListNode(sum % 10);
      curr = curr.next;
    
       -- Başta curr'a result atıldığı için, curr'un next'i değiştikçe result da değişiyor.
       -- curr, bir sonraki nodu takip eden bir referans olarak düşünülmeli.

---
+++ src/main/java/algorithms/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AddTwoNumbers {
+       public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode p = l1, q = l2, curr = result;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+       public static void main(String[] args) {
+        
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4, new ListNode(3));
+        
+        
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6, new ListNode(4));
+      
+        
+        AddTwoNumbers a = new AddTwoNumbers();
+    
+        ListNode result =  a.addTwoNumbers(l1, l2);
+        System.out.println("algorithms.AddTwoNumbers.main()");
+    }
+}
+ 
+  class ListNode {
+       int val;
+     ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+  
---
+++ src/main/java/algorithms/Test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Test {
+    public static void main(String[] args) {
+        Human ali = new Human();
+        ali.name = "Ali";
+        
+        Human muhammed = ali;
+        muhammed.name = "Muhammed";
+        
+        System.out.println(ali.name);
+    }
+}
+
+ class Human {
+    String name;
+    
+}
