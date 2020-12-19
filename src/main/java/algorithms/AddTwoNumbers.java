de0d367b0a0c9ff96ca908c19a281895424b056a
altayhunoglu <muratfair@gmail.com>
2020-12-19 23:29:01 +0300

    improves linked list.

--- src/main/java/algorithms/AddTwoNumbers.java
+++ src/main/java/algorithms/AddTwoNumbers.java
@@ -10,21 +10,19 @@ package algorithms;
  * @author ASUS
  */
 public class AddTwoNumbers {
-       public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       public ListNode addTwoNumbers(ListNode theFirstLinkedList, ListNode theSecondLinkedList) {
         ListNode result = new ListNode(0);
-           ListNode p = l1;
-           ListNode q = l2;
-           ListNode curr = result;
+        ListNode curr = result;
         int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
+        while (theFirstLinkedList != null || theSecondLinkedList != null) {
+            int x = (theFirstLinkedList != null) ? theFirstLinkedList.val : 0;
+            int y = (theSecondLinkedList != null) ? theSecondLinkedList.val : 0;
             int sum = carry + x + y;
             carry = sum / 10;
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+            if (theFirstLinkedList != null) theFirstLinkedList = theFirstLinkedList.next;
+            if (theSecondLinkedList != null) theSecondLinkedList = theSecondLinkedList.next;
         }
         if (carry > 0) {
             curr.next = new ListNode(carry);
@@ -44,7 +42,10 @@ public class AddTwoNumbers {
         AddTwoNumbers a = new AddTwoNumbers();
     
         ListNode result =  a.addTwoNumbers(l1, l2);
-        System.out.println("algorithms.AddTwoNumbers.main()");
+        
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = result;
+        LinkedList.printList(linkedList);
     }
 }
  
--- src/main/java/algorithms/LinkedList.java
+++ src/main/java/algorithms/LinkedList.java
@@ -1,61 +1,101 @@
 package algorithms;
- 
-public class LinkedList { 
 
-	ListNode head;  
-        
-   
- 
-	public static LinkedList insert(LinkedList list, int data) 
-	{  
-		ListNode new_node = new ListNode(data); 
-		new_node.next = null; 
- 
-		if (list.head == null) { 
-			list.head = new_node; 
-		} else { 
- 
-			ListNode last = list.head; 
-			while (last.next != null) { 
-				last = last.next; 
-			} 
- 
-			last.next = new_node; 
-		} 
- 
-		return list; 
-	} 
- 
-	public static void printList(LinkedList list) 
-	{ 
-		ListNode currNode = list.head; 
+public class LinkedList {
 
-		System.out.print("LinkedList: "); 
- 
-		while (currNode != null) { 
-		 
-			System.out.print(currNode.val + " "); 
+    ListNode head;
 
-		 
-			currNode = currNode.next; 
-		} 
-	} 
- 
-	public static void main(String[] args) 
-	{  
-		LinkedList list = new LinkedList(); 
- 
-		list = insert(list, 1); 
-		list = insert(list, 2); 
-		list = insert(list, 3); 
-		list = insert(list, 4); 
-		list = insert(list, 5); 
-		list = insert(list, 6); 
-		list = insert(list, 7); 
-		list = insert(list, 8); 
- 
-		printList(list); 
-	} 
-        
-        
-} 
+    public LinkedList insert(int data) {
+
+        ListNode new_node = new ListNode(data);
+        new_node.next = null;
+
+        if (this.head == null) {
+            this.head = new_node;
+        } else {
+
+            ListNode pointerToFindTheLast = this.head;
+            while (pointerToFindTheLast.next != null) {
+                pointerToFindTheLast = pointerToFindTheLast.next;
+            }
+
+            pointerToFindTheLast.next = new_node;
+        }
+
+        return this;
+    }
+
+    public LinkedList delete(int data) {
+        if (this.head != null) {
+
+            ListNode pointer = this.head;
+            
+            if (pointer.val == data) {
+                this.head = pointer.next;
+
+            } else {
+                while (pointer.next != null) {
+                    ListNode previous = pointer;
+                    pointer = pointer.next;
+
+                    if (pointer.val == data) {
+                        previous.next = pointer.next;
+                        break;
+                    }
+                }
+            } 
+        }
+        return this;
+    }
+
+    public LinkedList update(int data, int newData) { 
+        if (this.head != null) {
+
+            ListNode pointer = this.head;
+            
+            if (pointer.val == data) {
+                this.head.val = newData;
+
+            } else {
+                while (pointer.next != null) { 
+                    pointer = pointer.next;
+
+                    if (pointer.val == data) {
+                        pointer.val = newData;
+                        break;
+                    }
+                }
+            } 
+        }
+        return this;
+    }
+
+
+    public static void printList(LinkedList list) {
+        ListNode currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        while (currNode != null) {
+
+            System.out.print(currNode.val + " ");
+
+            currNode = currNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+
+        list.update(4, 9);
+
+        printList(list);
+    }
+
+}
