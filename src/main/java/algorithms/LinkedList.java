83a9fa636fa5351792e9afc4efe9c10efe68c681
altayhunoglu <muratfair@gmail.com>
2020-12-21 15:42:05 +0300

    LinkedList içerisine insert, delete, update, traverse methodları eklendi.

--- src/main/java/algorithms/LinkedList.java
+++ src/main/java/algorithms/LinkedList.java
@@ -1,101 +1,86 @@
 package algorithms;
-
+ 
 public class LinkedList {
 
-    ListNode head;
+    static ListNode head;
 
-    public LinkedList insert(int data) {
-
-        ListNode new_node = new ListNode(data);
-        new_node.next = null;
-
-        if (this.head == null) {
-            this.head = new_node;
-        } else {
-
-            ListNode pointerToFindTheLast = this.head;
-            while (pointerToFindTheLast.next != null) {
-                pointerToFindTheLast = pointerToFindTheLast.next;
-            }
-
-            pointerToFindTheLast.next = new_node;
+    static void insert(ListNode newNode) {
+        ListNode pointer = head;
+        
+        while (pointer.next!=null) {            
+            pointer = pointer.next;
         }
-
-        return this;
+        pointer.next = newNode;
     }
 
-    public LinkedList delete(int data) {
-        if (this.head != null) {
+    static void delete(int deleteVal) {
+        ListNode pointer = head;
+    
+         if(pointer.val == deleteVal){
+                 head = pointer.next;
+        } else{
+          while (pointer.next!=null) {  
+           ListNode previous  = pointer;
+            pointer = pointer.next;
+           
+            if(pointer.val == deleteVal){
+                previous.next = pointer.next;
+                break;
+            }    
+        }
+         }
+    }
 
-            ListNode pointer = this.head;
+   static void update(int updateVal, int newVal) {
+        ListNode pointer = head;
+    
+         if(pointer.val == updateVal){
+                 head.val = newVal;
+        } else{
+          while (pointer.next!=null) {  
+       
+            pointer = pointer.next;
+           
+            if(pointer.val == updateVal){
+                pointer.val = newVal;
+                break;
+            }    
+        }
+         }
+    }
+   
+
+    
+     static void traverse(ListNode head) {
+        if(head!=null){
+             ListNode pointer = head;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (pointer.next!=null) {    
+            sb.append(pointer.val).append(", ");
+            pointer = pointer.next;
             
-            if (pointer.val == data) {
-                this.head = pointer.next;
-
-            } else {
-                while (pointer.next != null) {
-                    ListNode previous = pointer;
-                    pointer = pointer.next;
-
-                    if (pointer.val == data) {
-                        previous.next = pointer.next;
-                        break;
-                    }
-                }
-            } 
         }
-        return this;
-    }
-
-    public LinkedList update(int data, int newData) { 
-        if (this.head != null) {
-
-            ListNode pointer = this.head;
-            
-            if (pointer.val == data) {
-                this.head.val = newData;
-
-            } else {
-                while (pointer.next != null) { 
-                    pointer = pointer.next;
-
-                    if (pointer.val == data) {
-                        pointer.val = newData;
-                        break;
-                    }
-                }
-            } 
+          sb.append(pointer.val);
+          
+          System.out.println(sb.toString());
         }
-        return this;
+       
     }
-
-
-    public static void printList(LinkedList list) {
-        ListNode currNode = list.head;
-
-        System.out.print("LinkedList: ");
-
-        while (currNode != null) {
-
-            System.out.print(currNode.val + " ");
-
-            currNode = currNode.next;
-        }
-    }
-
+    
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.insert(4);
-        list.insert(5);
-        list.insert(6);
-
-        list.update(4, 9);
-
-        printList(list);
+        head = new ListNode(27);
+ 
+        insert(new ListNode(28));
+ 
+        insert(new ListNode(29));
+ 
+        insert(new ListNode(30));
+     
+        update(29, 31);
+        
+        traverse(head);
     }
 
 }
--- src/main/java/algorithms/Test.java
+++ src/main/java/algorithms/Test.java
@@ -1,50 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algorithms;
 
-import java.util.Stack;
-
-/**
- *
- * @author ASUS
- */
 public class Test {
     public static void main(String[] args) {
-         Stack<String> stackStuff = new Stack();
-          
-         stackStuff.add("kitap");
-         stackStuff.add("elbise");
-         stackStuff.add("ayna");
-         
-       
-         Stack<String> stackFruit = new Stack();
-          
-         stackFruit.add("karpuz");
-         stackFruit.add("ceviz");
-         stackFruit.add("muz");
-         
-         
-         
-         Stack<Stack> matryoshka = new Stack();
+         Insan tufan = new Insan("Tufan");
+         Insan  tughan = new Insan("Tughan");
 
-            matryoshka.add(stackStuff);
-            matryoshka.add(stackFruit);
-            
-            
-          Stack<String> stack;
-         
-          while(!matryoshka.empty()){
-             stack = matryoshka.pop();
-              
-             while(!stack.empty()){
-                   String objectName = stack.pop();
-                   System.out.println(objectName);
-                }
-             
-         }
-         
+         tughan = tufan;
+       
+         System.out.println(tughan.name); 
+
      } 
+}
+
+
+class Insan {
+
+    public Insan(String name) {
+     this.name = name;
+    }
+    
+   String name; 
 }
