1c0f4117f29bcb6278664e2932afe98a778748f4
altayhunoglu <muratfair@gmail.com>
2020-12-25 01:11:07 +0300

    Adds BinaryTree

---
+++ src/main/java/algorithms/datastructures/Node.java
@@ -0,0 +1,14 @@
+package algorithms.datastructures;
+
+class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
