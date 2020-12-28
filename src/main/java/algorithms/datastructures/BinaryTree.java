6f7bdf4fd280ce9fcc70137d2a0d6fe24700a471
altayhunoglu <muratfair@gmail.com>
2020-12-28 22:58:22 +0300

    Binary Tree example.

--- src/main/java/algorithms/datastructures/BinaryTree.java
+++ src/main/java/algorithms/datastructures/BinaryTree.java
@@ -214,14 +214,22 @@ public class BinaryTree {
     }
     
     public static void main(String[] args) {
-        Node current = new Node(3);
+      
         
         BinaryTree binaryTree = new BinaryTree();
-        binaryTree.addRecursive(current, 5);
-        binaryTree.addRecursive(current, 2); 
-        
+       
+        binaryTree.add(5);
+        binaryTree.add(3); 
+        binaryTree.add(2);
+        binaryTree.add(1); 
+        binaryTree.add(4);
+        binaryTree.add(7);
+        binaryTree.add(6);
+        binaryTree.add(8); 
+        binaryTree.add(9);
         //binaryTree.traverseInOrder(current);
-        binaryTree.traversePreOrder(current);
+        
+        binaryTree.traverseLevelOrder();
       
     }
 }
