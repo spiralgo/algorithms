c7d79308951e7bd17c64b5d45d874a341bf98908
altayhunoglu <muratfair@gmail.com>
2021-02-06 14:55:57 +0300

    bugünkü 6 soru eklendi

--- src/main/java/algorithms/Playground.java
+++ src/main/java/algorithms/Playground.java
@@ -1,22 +1,11 @@
 package algorithms;
  
+ 
+
 public class Playground {
-    
-   static void add(int a, int b){
-        int result = a + b;
-        
-        System.out.println(result);
-    }
-    
+ 
     public static void main(String[] args) {
-        if(true){
-            System.out.println("1");
-        }else if(false){
-            System.out.println("2");
-        }else {
-             System.out.println("3");
-        }
-       
+ 
     } 
 }
 
