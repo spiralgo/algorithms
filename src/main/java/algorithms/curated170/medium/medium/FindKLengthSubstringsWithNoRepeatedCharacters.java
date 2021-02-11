81bf078a04f3acf9bb1dbb49f830b6edc5acc499
altayhunoglu <muratfair@gmail.com>
2021-02-11 13:01:37 +0300

    salı günü için 12 soru eklendi

---
+++ src/main/java/algorithms/curated170/medium/medium/FindKLengthSubstringsWithNoRepeatedCharacters.java
@@ -0,0 +1,28 @@
+package algorithms.curated170.medium.medium;
+
+import java.util.HashSet;
+
+ 
+public class FindKLengthSubstringsWithNoRepeatedCharacters {
+
+public int numKLenSubstrNoRepeats(String S, int K) {
+        HashSet<Character> cur = new HashSet<>();
+        int res = 0, i = 0;
+        for (int j = 0; j < S.length(); ++j) {
+            while (cur.contains(S.charAt(j)))
+                cur.remove(S.charAt(i++));
+            cur.add(S.charAt(j));
+            res += j - i + 1 >= K ? 1 : 0;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        FindKLengthSubstringsWithNoRepeatedCharacters f =
+                 new FindKLengthSubstringsWithNoRepeatedCharacters();
+        
+        int result =  f.numKLenSubstrNoRepeats("havefunonleetcode", 5);
+        
+        System.out.println(result);
+    }
+}
