d505fad82f0160395fa33bc90ee53cca969a9637
altayhunoglu <muratfair@gmail.com>
2021-02-17 23:32:58 +0300

    candy crush test

--- src/main/java/algorithms/Playground.java
+++ src/main/java/algorithms/Playground.java
@@ -5,11 +5,31 @@ import java.util.Arrays;
 public class Playground {
 
     public static void main(String[] args) {
-        String string = "thestoryofleetcodeandme";
+        int[][] in = {{1, 2},
+        {0, 2}};
 
-        int firstIndex = string.indexOf("story");
-        int secondIndex = "story".length() + firstIndex - 1;
-        System.out.println(firstIndex + ", " + secondIndex);
+        int low = in.length - 1;
+        int high = in.length-1;
 
+        while (high > 0) {
+            while (high > low && in[low][0]>0) {                
+                low--;
+            }
+                
+           while (in[high][0]>0) {                
+                high--;
+            }
+            in[low][0] = in[high][0];
+            in[high][0] = 0;
+        }
+        printArray(in);
     }
+
+    static void printArray(int[][] in) {
+        for (int i = 0; i < in.length; i++) {
+            System.out.println(Arrays.toString(in[i]));
+            System.out.println();
+        }
+    }
+
 }
