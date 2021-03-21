cfdf7e0bf2a789c3965eafe8094ee1d02e49b407
altayhunoglu <muratfair@gmail.com>
2021-03-22 02:38:14 +0300

    Merge origin/master
    Conflicts:
    	src/main/java/algorithms/Playground.java

--- src/main/java/algorithms/Playground.java
+++ src/main/java/algorithms/Playground.java
@@ -1,15 +1,12 @@
 package algorithms;
  
+ 
 public class Playground {
  
     public static void main(String[] args) {
  
-       Playground playground = new Playground();
-       int result = playground.lcm(3, 16);
-       
-       System.out.println(result);
  
     }
-    }
+     
 }
 
