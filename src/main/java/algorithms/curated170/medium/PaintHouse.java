e4513532fee07e899d1ad9a4fa50d4c14c8d0f3f
altayhunoglu <muratfair@gmail.com>
2021-04-18 19:36:59 +0300

    // Refer to: https://github.com/altayhunoglu/algorithms/issues/44#issuecomment-822001396

--- src/main/java/algorithms/curated170/medium/PaintHouse.java
+++ src/main/java/algorithms/curated170/medium/PaintHouse.java
@@ -1,29 +1,28 @@
-package algorithms;
+package algorithms.curated170.medium;
 
 import java.util.Arrays;
 import java.util.Random;
 
 public class PaintHouse {
-    static int solution(int[][] costs)
-    {
-        int h = costs.length; //number of houses
-        if(h==0)
-        {
-            return 0;
-        }
-        for(int i = 1; i<h;i++)
-        {
-            costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2]);
-            costs[i][1] += Math.min(costs[i-1][2], costs[i-1][0]);
-            costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
-        }
-        return Math.min(Math.min(costs[h-1][0], costs[h-1][1]), costs[h-1][2]);
-    }
+ 
+   static int minCost(int[][] costs)
+   {
+       int h = costs.length-1; //number of houses
+
+       for(int i = 1; i<=h;i++)
+       {
+           costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2]);
+           costs[i][1] += Math.min(costs[i-1][2], costs[i-1][0]);
+           costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
+       }
+       return Math.min(Math.min(costs[h][0], costs[h][1]), costs[h][2]);
+   }
+ 
     public static void main(String[] args) {
         int[][] costs0 = new int[][]{{1,3,7},{3,4,8},{5,1,11}};
         int[][] costs1 = new int[][]{{17,2,17},{16,16,5},{14,3,9}};
-        System.out.println(solution(costs0));
-        System.out.println(solution(costs1));
+        System.out.println(minCost(costs0));
+        System.out.println(minCost(costs1));
 
         //Testing for random input:
         int houses = 50;
@@ -35,6 +34,6 @@ public class PaintHouse {
             costs[i][2] = new Random().nextInt(50);
         }
         System.out.println(Arrays.deepToString(costs));
-        System.out.println(solution(costs));
+        System.out.println(minCost(costs));
     }
 }
