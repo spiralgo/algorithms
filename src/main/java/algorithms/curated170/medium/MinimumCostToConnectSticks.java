7adc0d9aebaf18693d55b9559be7c2ff393a630e
ErdemT09 <erdemturkoglu40@gmail.com>
2021-04-21 20:25:39 +0300

    Create MinimumCostToConnectSticks.java

---
+++ src/main/java/algorithms/curated170/medium/MinimumCostToConnectSticks.java
@@ -0,0 +1,27 @@
+package algorithms.curated170.medium;
+
+import java.util.PriorityQueue;
+
+public class MinimumCostToConnectSticks
+
+{
+    public static int connectSticks(int[] lengths) {
+        int cost = 0;
+
+        PriorityQueue<Integer> sticks = new PriorityQueue<Integer>();
+        for (int l : lengths) {
+            sticks.offer(l);
+        }
+        while (sticks.size() > 1) {
+            int newStick = sticks.poll() + sticks.poll(); //PriorityQueue polls the two smallest numbers
+            cost += (newStick);
+            sticks.offer(newStick); //We add the new connected sticks
+        }
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        int[] lengths = new int[] { 5, 2, 1, 3, 4, 7, 11 };
+        System.out.println(connectSticks(lengths)); // prints 84
+    }
+}
