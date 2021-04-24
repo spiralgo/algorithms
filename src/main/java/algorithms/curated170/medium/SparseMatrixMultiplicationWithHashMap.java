853109ca7281052f1977b9cc537d9f81fac9178a
ErdemT09 <erdemturkoglu40@gmail.com>
2021-04-24 14:10:22 +0300

    311-Added compress method

--- src/main/java/algorithms/curated170/medium/SparseMatrixMultiplicationWithHashMap.java
+++ src/main/java/algorithms/curated170/medium/SparseMatrixMultiplicationWithHashMap.java
@@ -11,24 +11,31 @@ public class SparseMatrixMultiplicationWithHashMap {
         int[][] C = new int[m][l];
         Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
 
-        for(int i=0; i< n; i++){
-            map.put(i, new HashMap<Integer, Integer>());
-            for(int j = 0; j<l; j++){
-                if(B[i][j] != 0){
-                    map.get(i).put(j, B[i][j]); //We store all numbers that are not zero.
-                }
-            }
-        }
-        for(int i=0; i< m; i++){
-            for(int j =0; j<n; j++){
-                if(A[i][j] != 0){
-                    for(int k : map.get(j).keySet()){
-                        C[i][k] += A[i][j]  * map.get(j).get(k); //We add these numbers to the positions in the output accordingly.
+        map = compress(B);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i][j] != 0) {
+                    for (int k : map.get(j).keySet()) {
+                        C[i][k] += A[i][j] * map.get(j).get(k); // We add these numbers to the positions in the output
+                                                                // accordingly.
                     }
                 }
             }
         }
         return C;
+    }
+
+    private static HashMap<Integer, Map<Integer, Integer>> compress(int[][] mat) {
+        HashMap<Integer, Map<Integer, Integer>> compressedMatrix = new HashMap<>();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != 0) {
+
+                    compressedMatrix.get(i).put(j, mat[i][j]);
+                }
+            }
+        }
+        return compressedMatrix;
     }
 
     public static void main(String[] args) {
