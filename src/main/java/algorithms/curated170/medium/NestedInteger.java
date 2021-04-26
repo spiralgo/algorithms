3a86acf99a2c6bf57a39253dee7c8525987868a1
ErdemT09 <erdemturkoglu40@gmail.com>
2021-04-26 12:39:19 +0300

    364-Nested Integer class

--- src/main/java/algorithms/curated170/medium/NestedInteger.java
+++ src/main/java/algorithms/curated170/medium/NestedInteger.java
@@ -1,32 +1,65 @@
 package algorithms.curated170.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
- 
-  // This is the interface that allows for creating nested lists.
-  // You should not implement it, or speculate about its implementation
-  public interface NestedInteger {
-      // Constructor initializes an empty nested list.
-      public void NestedInteger();
- 
-      // Constructor initializes a single integer.
-      public void NestedInteger(int value);
- 
-      // @return true if this NestedInteger holds a single integer, rather than a nested list.
-      public boolean isInteger();
- 
-      // @return the single integer that this NestedInteger holds, if it holds a single integer
-      // Return null if this NestedInteger holds a nested list
-      public Integer getInteger();
- 
-      // Set this NestedInteger to hold a single integer.
-      public void setInteger(int value);
- 
-      // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-      public void add(NestedInteger ni);
- 
-      // @return the nested list that this NestedInteger holds, if it holds a nested list
-      // Return empty list if this NestedInteger holds a single integer
-      public List<NestedInteger> getList();
-  }
- 
+class NestedInteger {
+    private List<NestedInteger> list;
+    private Integer integer;
+    
+    public NestedInteger(List<NestedInteger> list){
+        this.list = list;
+    }
+    
+    public void add(NestedInteger nestedInteger) {
+        if(this.list != null){
+            this.list.add(nestedInteger);
+        } else {
+            this.list = new ArrayList();
+            this.list.add(nestedInteger);
+        }
+    }
+
+    public void setInteger(int num) {
+        this.integer = num;
+    }
+
+    public NestedInteger(Integer integer){
+        this.integer = integer;
+    }
+
+    public NestedInteger() {
+        this.list = new ArrayList();
+    }
+
+    public boolean isInteger() {
+        return integer != null;
+    }
+
+    public Integer getInteger() {
+        return integer;
+    }
+
+    public List<NestedInteger> getList() {
+        return list;
+    }
+    
+    public String printNi(NestedInteger thisNi, StringBuilder sb){
+        if(thisNi.isInteger()) {
+            sb.append(thisNi.integer);
+            sb.append(",");
+        }
+        sb.append("[");
+        for(NestedInteger ni : thisNi.list){
+            if(ni.isInteger()) {
+                sb.append(ni.integer);
+                sb.append(",");
+            }
+            else {
+                printNi(ni, sb);
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
