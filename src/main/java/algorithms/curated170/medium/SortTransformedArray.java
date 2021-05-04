b9e679a2799230ae783c93819c7282999e3dfb68
altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>
2021-05-04 16:30:57 +0300

    Fixes to make more modular.

--- src/main/java/algorithms/curated170/medium/SortTransformedArray.java
+++ src/main/java/algorithms/curated170/medium/SortTransformedArray.java
@@ -2,44 +2,46 @@ package algorithms.curated170.medium;
 
 public class SortTransformedArray {
 
-    public class Solution {
+        int n, start = 0, end;
+        int[] res;
+        int[] nums;
+        int a, b, c;
 
         public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-            int n = nums.length, start = 0, end = n - 1;
-            int[] res = new int[n];
+            n = nums.length;
+            res = new int[n];
+            end = n-1;
+            this.nums = nums;
+            this.a = a;
+            this.b = b;
+            this.c = c;
             
             if (a > 0) {
                 for (int i = n - 1; i >= 0; i--) {
-                    int y1 = quadratic(nums[start], a, b, c);
-                    int y2 = quadratic(nums[end], a, b, c);
-                    if (y1 > y2) {
-                        res[i] = y1;
-                        start++;
-                    } else {
-                        res[i] = y2;
-                        end--;
-                    }
+                    quarticComparator(i, true);
                 }
             } else {
                 for (int i = 0; i < n; i++) {
-                    int y1 = quadratic(nums[start], a, b, c);
-                    int y2 = quadratic(nums[end], a, b, c);
-                    if (y1 < y2) {
-                        res[i] = y1;
-                        start++;
-                    } else {
-                        res[i] = y2;
-                        end--;
-                    }
+                    quarticComparator(i, false);
                 }
             }
 
             return res;
         }
 
-        int quadratic(int x, int a, int b, int c) {
+        void quarticComparator(int i, boolean pbType) {
+            int y1 = quadratic(nums[start]);
+            int y2 = quadratic(nums[end]);
+            if ((pbType && y1 > y2) || (!pbType && y1 < y2)) {
+                res[i] = y1;
+                start++;
+            } else {
+                res[i] = y2;
+                end--;
+            }
+        }
+
+        int quadratic(int x) {
             return a * x * x + b * x + c;
         }
-    }
-
 }
