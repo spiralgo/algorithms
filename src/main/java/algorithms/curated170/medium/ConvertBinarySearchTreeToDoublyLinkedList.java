f5afb07c127bb500eb8f150aeafa66106c52c59b
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-07 21:04:57 +0300

    426-Null fix + small Node class changes

--- src/main/java/algorithms/curated170/medium/ConvertBinarySearchTreeToDoublyLinkedList.java
+++ src/main/java/algorithms/curated170/medium/ConvertBinarySearchTreeToDoublyLinkedList.java
@@ -1,23 +1,22 @@
 package algorithms.curated170.medium;
 
-import algorithms.datastructures.TreeNode;
+import algorithms.datastructures.Node;
 
 public class ConvertBinarySearchTreeToDoublyLinkedList {
-    private TreeNode headPointer = new TreeNode(0);
-    private TreeNode lastNode = headPointer;
+    private Node headPointer = new Node(0);
+    private Node lastNode = headPointer;
 
-    public TreeNode treeToDoublyList(TreeNode root) {
-        if(root==null)
-        {
-            return null;
-        }
+    public Node treeToDoublyList(Node root) {
         convertTree(root);
         lastNode.right = headPointer.right;
-        headPointer.right.left = lastNode;
+        if(headPointer.right != null)
+        {
+            headPointer.right.left = lastNode;
+        }
         return headPointer.right;
     }
 
-    private void convertTree(TreeNode root) {
+    private void convertTree(Node root) {
         if (root == null) {
             return;
         }
@@ -29,17 +28,17 @@ public class ConvertBinarySearchTreeToDoublyLinkedList {
     }
 
     public static void main(String[] args) {
-        TreeNode t0 = new TreeNode(1);
-        TreeNode t1 = new TreeNode(4);
-        TreeNode t2 = new TreeNode(3, t0, t1);
-        TreeNode t3 = new TreeNode(7);
-        TreeNode t4 = new TreeNode(9);
-        TreeNode t5 = new TreeNode(8, t3, t4);
-        TreeNode root = new TreeNode(6, t2, t5);
-        TreeNode head = new ConvertBinarySearchTreeToDoublyLinkedList().treeToDoublyList(root);
+        Node t0 = new Node(1);
+        Node t1 = new Node(4);
+        Node t2 = new Node(3, t0, t1);
+        Node t3 = new Node(7);
+        Node t4 = new Node(9);
+        Node t5 = new Node(8, t3, t4);
+        Node root = new Node(6, t2, t5);
+        Node head = new ConvertBinarySearchTreeToDoublyLinkedList().treeToDoublyList(root);
         int i = 0;
         while (i<14) {
-            System.out.print(head.val + " ");
+            System.out.print(head.value + " ");
             head = head.right;
             i++;
         } //prints 1 3 4 6 7 8 9 1 3 4 6 7 8 9 , which shows that it loops through the circular doubly linked list.
