8548a8b3acde6e81c21a8bfe8977b13d55dc0ded
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-10 21:03:38 +0300

    1151-Fixed false incrementation

--- src/main/java/algorithms/curated170/medium/MinimumSwaps.java
+++ src/main/java/algorithms/curated170/medium/MinimumSwaps.java
@@ -17,24 +17,27 @@ public class MinimumSwaps {
 
             int minChanges = 0;
             for (int i = 0; i < windowSize; i++) {
+                  System.out.println(i + " " + data[i]);
                   if (data[i] == 0) {
                         minChanges++;
                   }
             }
 
+            System.out.println(windowSize + " " + minChanges);
             int prev = minChanges;
             for (int l = 1, r = windowSize; l <= data.length - windowSize; l++, r++) {
-                  int a = data[l], b = data[r];
-                  
+                  int a = data[l-1], b = data[r];
+                  System.out.println("index: " + l + " prev: " + prev + " a: " + a + " b: " + b + " minChanges: " + minChanges);
                   if (a == b) {
                         continue;
                   }
                   
                   if (a > b) {
-                        prev--;
-                  } else if (a < b) {
                         prev++;
+                  } else if (a < b) {
+                        prev--;
                   }
+                  System.out.println(prev);
                   minChanges = Math.min(minChanges, prev);
             }
             return minChanges;
@@ -43,6 +46,6 @@ public class MinimumSwaps {
       public static void main(String[] args) {
 
             var solution = new MinimumSwaps();
-            System.out.println(solution.minimumSwaps(new int[] { 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1}));
+            System.out.println(solution.minimumSwaps(new int[] {1,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1}));
       }
 }
