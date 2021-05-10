5422e621299637a74f05923b01aadea1df37b36c
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-10 16:12:58 +0300

    249-A small change suggestion

--- src/main/java/algorithms/curated170/medium/GroupShiftedStrings.java
+++ src/main/java/algorithms/curated170/medium/GroupShiftedStrings.java
@@ -5,31 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
- 
 public class GroupShiftedStrings {
- 
-  public List<List<String>> groupStrings(String[] strings) {
-    Map<String, List<String>> map = new HashMap<>();
 
-    for(String s : strings) {
-        String key = getKey(s);
-        List<String> list = map.getOrDefault(key, new ArrayList<>());
-        list.add(s);
-        map.put(key, list);
-    }
-    return new ArrayList<>(map.values());
-}
+    public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> map = new HashMap<>();
 
-private String getKey(String s) {
-    char[] chars = s.toCharArray();
-    String key = "";
-    for(int i = 1; i < chars.length; i++) {
-        int diff = chars[i] - chars[i-1];
-        key += diff < 0 ? diff + 26 : diff;
-        key += ",";
+        for (String s : strings) {
+            String key = getKey(s);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
     }
-    return key;
-}
+
+    private String getKey(String s) {
+        char[] chars = s.toCharArray();
+        String key = "";
+        char firstChar = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            int diff = chars[i] - firstChar;
+            key += diff < 0 ? diff + 26 : diff;
+            key += ",";
+        }
+        return key;
+    }
 
     public static void main(String[] args) {
         GroupShiftedStrings groupShiftedStrings = new GroupShiftedStrings();
