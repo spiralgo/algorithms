7d7d762d93dbab2c89fbc7e23083b1a9e3f06250
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-10 19:32:37 +0300

    1272-Initial solution

---
+++ src/main/java/algorithms/curated170/medium/RemoveInterval.java
@@ -0,0 +1,34 @@
+package algorithms.curated170.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class RemoveInterval {
+
+    public int[][] solution(int[][] intervals, int[] toBeRemoved) {
+        List<int[]> intervals_ = new ArrayList<>();
+        int start = toBeRemoved[0], end = toBeRemoved[1];
+        for (int[] interval : intervals) {
+            
+            if (interval[1] <= start || interval[0] >= end) {
+                intervals_.add(interval);
+                continue;
+            }
+            
+            if (interval[0] < start) {
+                intervals_.add(new int[]{interval[0], start})
+            }
+            if (interval[1] > end) {
+                intervals_.add(new int[]{end, interval[1]})
+            }
+
+        }
+        return (int[][]) intervals_.toArray();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
