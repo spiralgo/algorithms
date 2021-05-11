47d60e34bfee9c3be1d98d295f32247caa564b5d
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-11 09:48:47 +0300

    1151-Refactoring patch

--- src/main/java/algorithms/curated170/medium/MinimumSwaps.java
+++ src/main/java/algorithms/curated170/medium/MinimumSwaps.java
@@ -1,43 +1,27 @@
 package algorithms.curated170.medium;
 
 public class MinimumSwaps {
-      private int[] data;
-      private int windowSize = 0;
 
-      public int minimumSwaps(int[] data) {
-            this.data = data;
+      public int minSwaps(int[] data) {
+            int windowSize = 0;
+
             for (int i : data) {
-                  if (i == 1) {
-                        windowSize++;
-                  }
-            }
-            if (windowSize == 0 || windowSize == 1) {
-                  return 0;
+                  windowSize += i;
             }
 
             int minChanges = 0;
             for (int i = 0; i < windowSize; i++) {
-                  System.out.println(i + " " + data[i]);
-                  if (data[i] == 0) {
-                        minChanges++;
-                  }
+
+                  minChanges += (1 - data[i]);
+
             }
 
-            System.out.println(windowSize + " " + minChanges);
             int prev = minChanges;
-            for (int l = 1, r = windowSize; l <= data.length - windowSize; l++, r++) {
-                  int a = data[l-1], b = data[r];
-                  System.out.println("index: " + l + " prev: " + prev + " a: " + a + " b: " + b + " minChanges: " + minChanges);
-                  if (a == b) {
-                        continue;
-                  }
-                  
-                  if (a > b) {
-                        prev++;
-                  } else if (a < b) {
-                        prev--;
-                  }
-                  System.out.println(prev);
+
+            for (int i = windowSize; i < data.length; i++) {
+
+                  prev += data[i - windowSize] - data[i];
+
                   minChanges = Math.min(minChanges, prev);
             }
             return minChanges;
@@ -46,6 +30,7 @@ public class MinimumSwaps {
       public static void main(String[] args) {
 
             var solution = new MinimumSwaps();
-            System.out.println(solution.minimumSwaps(new int[] {1,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1}));
+            System.out.println(solution.minimumSwaps(new int[] { 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0,
+                        1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1 }));
       }
 }
