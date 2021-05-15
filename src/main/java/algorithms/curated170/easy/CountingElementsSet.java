9ad386df204e63c916f2c86b2c22ae024e904c0f
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-15 12:32:53 +0300

    1426-Two alternative solutions

---
+++ src/main/java/algorithms/curated170/easy/CountingElementsSet.java
@@ -0,0 +1,52 @@
+package algorithms.curated170.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountingElementsSet {
+    public int countElements(int[] arr) {
+
+        Set<Integer> nums = new HashSet<>();
+        int count = 0;
+        for (int n : arr) {
+            nums.add(n);
+        }
+        for (int n : arr) {
+            if (nums.contains(n + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        var solution0 = new CountingElementsSet();
+        var solution1 = new CountingElements();
+
+        int[] arr = { 1, 2, 3 };
+        System.out.println(solution0.countElements(arr));
+        System.out.println(solution1.countElements(arr));
+        System.out.println("#");
+
+        arr = new int[] { 1, 1, 2, 2 };
+        System.out.println(solution0.countElements(arr));
+        System.out.println(solution1.countElements(arr));
+        System.out.println("#");
+
+        arr = new int[] { 1, 1, 2 };
+        System.out.println(solution0.countElements(arr));
+        System.out.println(solution1.countElements(arr));
+        System.out.println("#");
+
+        arr = new int[] { 1, 1, 3, 3, 5, 5, 7, 7 };
+        System.out.println(solution0.countElements(arr));
+        System.out.println(solution1.countElements(arr));
+        System.out.println("#");
+
+        arr = new int[] { 1, 3, 2, 3, 5, 0 };
+        System.out.println(solution0.countElements(arr));
+        System.out.println(solution1.countElements(arr));
+        System.out.println("#");
+    }
+}
