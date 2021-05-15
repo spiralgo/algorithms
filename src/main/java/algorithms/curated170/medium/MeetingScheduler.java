ccaf16a758eac18ac3ddb1b513694e0c2930d979
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-15 19:54:24 +0300

    1229 - LeetCode fixes

--- src/main/java/algorithms/curated170/medium/MeetingScheduler.java
+++ src/main/java/algorithms/curated170/medium/MeetingScheduler.java
@@ -2,10 +2,12 @@ package algorithms.curated170.medium;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 class MeetingScheduler {
-    public int[] scheduleMeeting(int[][] p1, int[][] p2, int duration) {
+    List<Integer> minAvailableDuration (int[][] p1, int[][] p2, int duration) {
 
         Comparator<int[]> c = new Comparator<int[]>() {
             @Override
@@ -19,8 +21,7 @@ class MeetingScheduler {
 
         int i1 = 0, i2 = 0;
         while (i1 < p1.length && i2 < p2.length) {
-            int s1 = p2[i1][0], e1 = p2[i1][1], s2 = p2[i2][0], e2 = p2[i2][1]; // start and end times of the respective
-                                                                                // persons at the current indices
+            int s1 = p2[i1][0], e1 = p2[i1][1], s2 = p2[i2][0], e2 = p2[i2][1]; // start and end times of the respective persons at this interva
 
             if (s2 >= e1) {
                 i1++;
@@ -28,15 +29,15 @@ class MeetingScheduler {
                 i2++;
             } else if (s1 <= s2) {
                 if ((e2 <= e1 && e2 - s2 >= duration) || (e1 <= e2 && e1 - s2 >= duration)) {
-                    return new int[] { s2, s2 + duration };
+                    return List.of(s2, s2 + duration );
                 }
             } else if (s2 <= s1) {
                 if ((e1 <= e2 && e1 - s1 >= duration) || (e2 <= e1 && e2 - s1 >= duration)) {
-                    return new int[] { s1, s1 + duration };
+                    return List.of( s1, s1 + duration );
                 }
             }
         }
-        return new int[] {}; // If doesn't work, return empty array.
+        return Collections.emptyList(); // If doesn't work, return empty array.
     }
 
     public static void main(String[] args) {
