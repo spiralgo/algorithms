a04c667450cf0a305190cdc4e8c5d3e84eb2c4d2
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-15 15:29:10 +0300

    1229-Initial solution

---
+++ src/main/java/algorithms/curated170/medium/MeetingScheduler.java
@@ -0,0 +1,61 @@
+package algorithms.curated170.medium;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class MeetingScheduler {
+    public int[] scheduleMeeting(int[][] p1, int[][] p2, int duration) {
+
+        Comparator<int[]> c = new Comparator<int[]>() { 
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        };
+
+        Arrays.sort(p1, c); 
+        Arrays.sort(p2, c);
+        
+        int i1 = 0, i2 = 0;
+        while(i1<p1.length && i2<p2.length)
+        {
+            int s1 = p2[i1][0], e1 = p2[i1][1], s2 = p2[i2][0], e2 = p2[i2][1];
+            if(s2 >= e1)
+            {
+                i1++;
+            }
+            else if(s1 >= e2 )
+            {
+                i2++;
+            }
+            else if(s1 <= s2 )
+            {
+                if( e2 <= e1 && e2-s2 >= duration )
+                {
+                    return new int[]{s2, s2+duration};
+                }
+                else if(e1 <= e2 && e1-s2 >= duration)
+                {
+                    return new int[]{s2, s2+duration};
+                }
+            }
+            else if(s2 <= s1)
+            {
+                if( e1 <= e2 && e1-s1 >= duration )
+                {
+                    return new int[]{s1, s1+duration};
+                }
+                else if(e2 <= e1 && e2-s1 >= duration)
+                {
+                    return new int[]{s1, s1+duration};
+                }
+            }
+        }
+        return new int[]{}; //If doesn't work, return empty array.
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
