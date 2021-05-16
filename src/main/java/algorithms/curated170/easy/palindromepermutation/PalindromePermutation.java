e6594a839449202d086378b3a6575f78a7361d97
altayhunoglu <muratfair@gmail.com>
2021-05-16 23:53:59 +0300

    Adds faster version.

--- src/main/java/algorithms/curated170/easy/palindromepermutation/PalindromePermutation.java
+++ src/main/java/algorithms/curated170/easy/palindromepermutation/PalindromePermutation.java
@@ -1,4 +1,4 @@
-package algorithms.curated170.easy;
+package algorithms.curated170.easy.palindromepermutation;
  
 import java.util.HashMap;
 
---
+++ src/main/java/algorithms/curated170/easy/palindromepermutation/PalindromePermutationFaster.java
@@ -0,0 +1,27 @@
+package algorithms.curated170.easy.palindromepermutation;
+
+ 
+public class PalindromePermutationFaster {
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        // 
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        int[] map = new int[128];
+        int count = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            map[s.charAt(i)] ++;
+        }
+        for (int key = 0; key < map.length; key ++) {
+            if (count <= 1) {
+                count += map[key] % 2;
+            } else {
+                break;
+            }
+
+        }
+        return count <= 1;
+    }
+}
+}
