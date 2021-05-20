3798b7d16abd9819ef2a64f6f876275538340415
altayhunoglu <muratfair@gmail.com>
2021-05-20 06:39:52 +0300

    Adds the solution without concationation.

--- src/main/java/algorithms/curated170/easy/sentencesimilarity/SentenceSimilarity.java
+++ src/main/java/algorithms/curated170/easy/sentencesimilarity/SentenceSimilarity.java
@@ -21,4 +21,20 @@ public class SentenceSimilarity {
         }
         return true;
     }
+  public boolean areSentencesSimilarWithoutConcatination(
+            String[] words1, String[] words2,List<List<String>> pairs) {
+        if (words1.length != words2.length) return false;
+
+        Set<List<String>> pairset = new HashSet<>();
+        for (List<String> pair: pairs)
+            pairset.add(pair);
+
+        for (int i = 0; i < words1.length; ++i) {
+            if (!words1[i].equals(words2[i]) &&
+                    !pairset.contains(List.of(words1[i], words2[i])) &&
+                    !pairset.contains(List.of(words2[i], words1[i])))
+                return false;
+        }
+        return true;
+    }
 }
