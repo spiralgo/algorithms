bc17fd77171a08c81dbafb907b5a063f70c194ae
altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>
2021-05-21 03:41:49 +0300

    Update NumberOfCornerRectangles.java
    Adds the row-first approach as a new method.

--- src/main/java/algorithms/curated170/medium/NumberOfCornerRectangles.java
+++ src/main/java/algorithms/curated170/medium/NumberOfCornerRectangles.java
@@ -32,4 +32,19 @@ public class NumberOfCornerRectangles {
         }
         return totalCount;
     }
+ 
+ public int  countCornerRectanglesRowFirstApproach(int[][] grid) {
+        int totalCount= 0;
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = i + 1; j < grid.length; j++) {
+                int count = 0;
+                for (int k = 0; k < grid[0].length; k++) {
+                    if (grid[i][k] == 1 && grid[j][k] == 1) count++;
+                }
+                if (count > 0) totalCount+= count * (count - 1) / 2;
+            }
+        }
+        return totalCount;
+    }
+ 
 }
