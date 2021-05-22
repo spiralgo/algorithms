b201c6c1455b98f4d3a659f9f639788fff150311
altayhunoglu <muratfair@gmail.com>
2021-05-22 12:40:01 +0300

    Adds our first unit test for The Maze - DFS solution.

---
+++ src/test/java/algorithms/curated170/medium/maze/TheMazeDFSTest.java
@@ -0,0 +1,101 @@
+package algorithms.curated170.medium.maze;
+ 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Altay
+ */
+public class TheMazeDFSTest {
+    int[][] directions = {{0, -1}, {1, 0}, {-1, 0}, {0, 1}};
+    int[][] maze;
+    int[] destination;
+    int[] start;
+    boolean visited[][];
+    
+    @Before
+    public void setUp() {
+        int[][] maze = {{0, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 1, 0},
+                        {1, 1, 0, 1, 1},
+                        {0, 0, 0, 0, 0}};
+        int[] start = {0, 4}, destination = {4, 4};
+         visited = new boolean[maze.length][maze[0].length];
+         this.maze = maze;
+         this.destination = destination;
+         this.start = start;
+    }
+ 
+
+    /**
+     * Test of hasPath method, of class TheMazeDFS.
+     */
+    @Test
+    public void testHasPath() {
+        System.out.println("hasPath");
+        TheMazeDFS instance = new TheMazeDFS();
+        instance.destination = destination;
+        boolean expResult = true;
+        boolean result = instance.hasPath(maze, start, destination);
+        assertEquals(expResult, result);
+    
+        
+    }
+
+    /**
+     * Test of dps method, of class TheMazeDFS.
+     */
+    @Test
+    public void testDps() {
+        System.out.println("dps");
+     
+        TheMazeDFS instance = getFreshInstance(new int[]{4, 4});
+          
+        boolean result = instance.dps(0, 4);
+        assertEquals(true, result);
+     
+        instance = getFreshInstance(new int[]{1, 2});
+        result = instance.dps(0, 4);
+        assertEquals(true, result);
+     
+        instance = getFreshInstance(new int[]{3, 2});
+        result = instance.dps(0, 4);
+        assertEquals(false, result);
+    }
+   TheMazeDFS getFreshInstance(int[] destination){
+          TheMazeDFS instance = new TheMazeDFS();
+          instance.maze = maze;
+          instance.visited = new boolean[maze.length][maze[0].length];
+          instance.destination = destination;
+          
+          return instance;
+                  
+   }
+    /**
+     * Test of canPass method, of class TheMazeDFS.
+     */
+    @Test
+    public void testCanPass() {
+        System.out.println("canPass");
+      
+        TheMazeDFS instance = getFreshInstance(destination);
+      
+      
+        int nextX = 0;
+        int nextY = 0;
+        boolean result = instance.canPass(nextX, nextY);
+        
+        assertEquals(true, result);
+        
+         nextX = 0;
+         nextY = 2;
+         result = instance.canPass(nextX, nextY);
+         assertNotEquals(true, result);
+
+    
+    }
+    
+}
