77d7d8ec61527014476741601a6a9c68c57352c4
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-28 16:47:09 +0300

    250 - Add initial solution

---
+++ src/main/java/algorithms/curated170/medium/CountUnivalueSubtrees.java
@@ -0,0 +1,33 @@
+package algorithms.curated170.medium;
+
+import algorithms.datastructures.TreeNode;
+
+public class CountUnivalueSubtrees {
+    
+    private int count = 0;
+
+    public int countUnivalSubtrees(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        isUnivalSubtree(root);
+        return count;
+    }
+
+    private boolean isUnivalSubtree(TreeNode root) {
+
+        boolean isUnival = true;
+        if (root.left != null) {
+            isUnival = root.left.val == root.val && isUnivalSubtree(root.left);
+        }
+        if (root.left != null && isUnival) {
+            isUnival = root.right.val == root.val && isUnivalSubtree(root.right);
+        }
+        if (isUnival) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+
+}
