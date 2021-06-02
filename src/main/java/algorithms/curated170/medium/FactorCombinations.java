8b88f6550533aa95d4a9ad27d2eecba1985da63e
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-02 18:26:44 +0300

    254 - Add working solution

--- src/main/java/algorithms/curated170/medium/FactorCombinations.java
+++ src/main/java/algorithms/curated170/medium/FactorCombinations.java
@@ -3,127 +3,48 @@ package algorithms.curated170.medium;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 import javax.swing.plaf.TreeUI;
 import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
 
-public class FactorCombinations
-{
-
-    List<List<Integer>> ans = new ArrayList<>();
-    Set<Integer> hasFactor = new HashSet<>();
-    int limitPrime;
-
-    List<Integer> primes = new ArrayList<>(List.of(2,3,5,7));
-
-    boolean sleeveFulled = false;
-    boolean[] sleeve;
-    List<Integer> primeFactors = new ArrayList<>();
-
-
-    int orig;
-
-    public List<Integer> generatePrimeFactors(int num)
-    {
-        orig = num;
-        limitPrime = (int) Math.sqrt(num);
-
-        sleeve = new boolean[limitPrime+1];
-
-        if(limitPrime < 11) sleeveFulled = true;
-        help(num, 0, 1);
-
-        //System.out.println(Arrays.toString(sleeve));
-        return primeFactors;
-    }
-    
-    private boolean isPrime(int n)
-    {
-        for(int p : primes)
-        {
-            if(n%p == 0)
-            {
-                return false;
+public class FactorCombinations {
+    void backtracking(int n, int index, Stack<Integer> temp, List<List<Integer>> res) {
+        if (n > 1) {
+            if (temp.size() >= 1) {
+                temp.add(n);
+                res.add(new ArrayList<>(temp));
+                temp.pop();
             }
         }
-        return sleeveFulled;
-    }
-    private void help(int n, int ip, int prev)
-    {
-        //System.out.println("n: " + n+ " ip: " + ip +" prev: " + prev);
-        if(prev == orig)
-        {
-            return;
-        }
-        if(isPrime(n))
-        {
-            primeFactors.add(n);
-            return;
-        }
-        int prime = getPrime(ip);
-        if(n<2 || prime <= 0)
-        {
-            return;
-        }
-        if(n % prime == 0)
-        {
-            primeFactors.add(prime);
-            help(n/prime, ip, prev*prime);
-        }
-        else
-        {
-            primes.set(ip, -1*prime);
-            help(n, ip+1, prev);
+        for (int i = index; i < n; ++i) {
+            if (i > n / i)
+                break;
+            if (n % i == 0) {
+                temp.add(i);
+                backtracking(n / i, i, temp, res);
+                temp.pop();
+            }
         }
     }
 
-    private int getPrime(int ip)
-    {
-        if(primes.size() > ip)
-        {
-            return primes.get(ip);
-        }
-
-        for(int p : primes)
-        {
-            p = Math.abs(p);
-            if(p==2)  
-            {
-                continue;
-            }
-
-            if(sleeve[p])
-            {
-                continue;
-            }
-
-            for(int i = p; i<sleeve.length; i+=p){
-                sleeve[i] = true;
-            }
-        }
-
-        int firstP = -1;
-        for(int i = 3; i<sleeve.length; i+=2)
-        {
-            if(!sleeve[i])
-            {
-                primes.add(i);
-                firstP = firstP < 0 ? i : firstP;
-            }
-        }
-        sleeveFulled = true;
-        return firstP;
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n < 4)
+            return res;
+        Stack<Integer> temp = new Stack<>();
+        backtracking(n, 2, temp, res);
+        return res;
     }
-    
+
     public static void main(String[] args) {
         var solution = new FactorCombinations();
-
-        for(int i = 0; i< (1<<7); i++)
-        {
-            System.out.println("Factors of " + i + ": " + new FactorCombinations().generatePrimeFactors(i));
+        for (int i = 0; i < (1 << 5); i++) {
+            System.out.println("Factors of " + i + ": " + new FactorCombinations().getFactors(i));
         }
     }
 }
