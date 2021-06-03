82439fca2c645a863e8915472b87318880bb7e94
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-03 18:09:06 +0300

    254-Fix style 2

--- src/main/java/algorithms/curated170/medium/FactorCombinations.java
+++ src/main/java/algorithms/curated170/medium/FactorCombinations.java
@@ -6,36 +6,36 @@ import java.util.Stack;
 
 public class FactorCombinations {
 
-    List<List<Integer>> res;
-    Stack<Integer> temp;
+    List<List<Integer>> combinations;
+    Stack<Integer> currFactors;
 
     void backtracking(int n, int index) {
         if (n > 1) {
-            if (!temp.isEmpty()) {
-                temp.add(n);
-                res.add(new ArrayList<>(temp));
-                temp.pop();
+            if (!currFactors.isEmpty()) {
+                currFactors.add(n);
+                combinations.add(new ArrayList<>(currFactors));
+                currFactors.pop();
             }
         }
 
         for (int i = index; i <= n / i; i++) {
             if (n % i == 0) {
-                temp.add(i);
+                currFactors.add(i);
                 backtracking(n / i, i);
-                temp.pop();
+                currFactors.pop();
             }
         }
     }
 
     public List<List<Integer>> getFactors(int n) {
-        res = new ArrayList<>();
+        combinations = new ArrayList<>();
         if (n < 4)
         {
-            return res;
+            return combinations;
         }
-        temp = new Stack<>();
+        currFactors = new Stack<>();
         backtracking(n, 2);
-        return res;
+        return combinations;
     }
 
     public static void main(String[] args) {
