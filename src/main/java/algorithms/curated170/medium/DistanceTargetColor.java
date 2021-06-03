6b72e5703dacbf6a05c88717a3a529ac0adf9c10
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-03 20:58:40 +0300

    1182-Add look up-linear and binary search solutions

--- src/main/java/algorithms/curated170/medium/DistanceTargetColor.java
+++ src/main/java/algorithms/curated170/medium/DistanceTargetColor.java
@@ -1,72 +1,118 @@
 package algorithms.curated170.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import javax.print.attribute.standard.Sides;
-
 public class DistanceTargetColor {
 
-    private static final int BLUE = 1;
-    private static final int GREEN = 2;
-    private static final int RED = 3;
+    private static final int NOT_FOUND = Integer.MAX_VALUE;
+    int[][] closestDistTable;
 
-    private List<Integer> blues = new ArrayList<>();
-    private List<Integer> greens = new ArrayList<>();
-    private List<Integer> reds = new ArrayList<>();
+    public List<Integer> sDC(int[] colors, int[][] queries) {
+
+        closestDistTable = new int[colors.length][];
+        int[] nearest = new int[4];
+        Arrays.fill(nearest, NOT_FOUND);
+
+        assignClosestLeftColors(colors, nearest);
+
+        compareClosestRightColors(colors);
+
+        return assignClosestDistances(queries);
+    }
+
+    private List<Integer> assignClosestDistances(int[][] queries) {
+
+        List<Integer> distList = new ArrayList<>();
+
+        for (int[] query : queries) {
+            int dist = closestDistTable[query[0]][query[1]];
+
+            if (dist == NOT_FOUND) {
+                dist = -1;
+            }
+
+            distList.add(dist);
+        }
+
+        return distList;
+    }
+
+    private void compareClosestRightColors(int[] colors) {
+        for (int i = colors.length - 2; i >= 0; i--) {
+            for (int c = 1; c <= 3; c++) {
+                closestDistTable[i][c] = Math.min(closestDistTable[i][c], closestDistTable[i + 1][c] + 1);
+            }
+        }
+    }
+
+    private void assignClosestLeftColors(int[] colors, int[] nearest) {
+        for (int i = 0; i < colors.length; i++) {
+            nearest[colors[i]] = 0;
+
+            for (int j = 1; j <= 3; j++) {
+                if (nearest[j] != NOT_FOUND && j != colors[i]) {
+                    nearest[j]++;
+                }
+            }
+
+            closestDistTable[i] = nearest.clone();
+        }
+    }
+
+    private List<List<Integer>> look;
 
     public List<Integer> shortestDistanceColor(int[] colors, int[][] queries) {
+        look = List.of(Collections.emptyList(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
         for (int i = 0; i < colors.length; i++) {
             int color = colors[i];
-            switch (color) {
-                case BLUE:
-                    blues.add(i);
-                    break;
-                case GREEN:
-                    greens.add(i);
-                    break;
-                case RED:
-                    reds.add(i);
-                    break;
-            }
+            look.get(color).add(i);
+        }
+        System.out.println(look);
+        List<Integer> ret = new ArrayList<>();
+
+        final boolean[] hasColor = new boolean[] { false, !look.get(1).isEmpty(), !look.get(2).isEmpty(),
+                !look.get(3).isEmpty() };
+
+        for (int[] query : queries) {
+            int idx = query[0];
+            int c = query[1];
+            if (hasColor[c])
+                ret.add(Math.abs(idx - binarySearch(idx, look.get(c))));
+            else
+                ret.add(-1);
+        }
+        System.out.println(ret);
+        return ret;
+    }
+
+    private int binarySearch(int value, List<Integer> list) {
+        if (value < list.get(0)) {
+            return list.get(0);
+        }
+        if (value > list.get(list.size() - 1)) {
+            return list.get(list.size() - 1);
         }
 
-        List<Integer> dists = new ArrayList<>();
-        System.out.println(blues + "" + greens + "" + reds);
-        for (int[] pair : queries) {
-            int idx = pair[0];
-            int searched = pair[1];
-            int sIdx = -1;
-            switch (searched) {
-                case BLUE:
-                    sIdx = Collections.binarySearch(blues, idx);
-                    break;
-                case GREEN:
-                    sIdx = Collections.binarySearch(greens, idx);
-                    break;
-                case RED:
-                    sIdx = Collections.binarySearch(reds, idx);
-                    break;
-            }
-            if (sIdx < 0) {
-                sIdx = -1 * (sIdx + 1);
-            }
-            switch (searched) {
-                case BLUE:
-                    sIdx = sIdx == blues.size() ? blues.get(sIdx - 1) : blues.get(sIdx);
-                    break;
-                case GREEN:
-                    sIdx = sIdx == greens.size() ? greens.get(sIdx - 1) : greens.get(sIdx);
-                    break;
-                case RED:
-                    sIdx = sIdx == reds.size() ? reds.get(sIdx - 1) : reds.get(sIdx);
-                    break;
-            }
-            dists.add(sIdx);
-        }
+        int lo = 0;
+        int hi = list.size() - 1;
 
-        return dists;
+        while (lo <= hi) {
+            int mid = (hi + lo) / 2;
+
+            if (value < list.get(mid)) {
+                hi = mid - 1;
+            } else if (value > list.get(mid)) {
+                lo = mid + 1;
+            } else {
+                return list.get(mid);
+            }
+        }
+        // lo == hi + 1
+        return (list.get(lo) - value) < (value - list.get(hi)) ? list.get(lo) : list.get(hi);
     }
 
     public static void main(String[] args) {
@@ -74,7 +120,8 @@ public class DistanceTargetColor {
 
         int[] colors = new int[] { 1, 1, 2, 1, 3, 2, 2, 3, 3 };
         int[][] queries = new int[][] { { 1, 3 }, { 2, 2 }, { 6, 1 } };
-        System.out.println(solution.shortestDistanceColor(colors, queries));
+        System.out.println(solution.sDC(colors, queries));
+        solution.shortestDistanceColor(colors, queries);
 
     }
 }
