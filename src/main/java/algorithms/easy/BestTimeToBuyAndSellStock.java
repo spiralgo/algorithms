5a865570389d7a3fb1c4879d8967ab6f83c5536a
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-05 11:27:44 +0300

    121-Improve Logic

--- src/main/java/algorithms/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/algorithms/easy/BestTimeToBuyAndSellStock.java
@@ -5,17 +5,20 @@ public class BestTimeToBuyAndSellStock {
 	public int maxProfit(int[] prices) {
 		int currBuy = Integer.MAX_VALUE, maxProfit = 0;
 		for (int p : prices) {
-			currBuy = Math.min(p, currBuy);
-			maxProfit = Math.max(maxProfit, p - currBuy);
+			if (p > currBuy) {
+				currBuy = p;
+			} else {
+				maxProfit = Math.max(maxProfit, p - currBuy);
+			}
 		}
 		return maxProfit;
 	}
 
 	public static void main(String[] args) {
 		var solution = new BestTimeToBuyAndSellStock();
-		int[] prices = new int[]{7,1,5,3,6,4};
-		System.out.println(solution.maxProfit(prices));  // prints 5 (6-1)
-		prices = new int[]{7,6,4,3,1};
-		System.out.println(solution.maxProfit(prices));  // prints 0
+		int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+		System.out.println(solution.maxProfit(prices)); // prints 5 (6-1)
+		prices = new int[] { 7, 6, 4, 3, 1 };
+		System.out.println(solution.maxProfit(prices)); // prints 0
 	}
 }
