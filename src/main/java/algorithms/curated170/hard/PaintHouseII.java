10678f4d0bd00250fdf202c4e822920c4d166cd8
altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>
2021-06-05 17:49:25 +0300

    Update PaintHouseII.java

--- src/main/java/algorithms/curated170/hard/PaintHouseII.java
+++ src/main/java/algorithms/curated170/hard/PaintHouseII.java
@@ -3,17 +3,16 @@ package algorithms.curated170.hard;
 public class PaintHouseII {
 
     public int minCostII(int[][] costs) {
-        int houses = costs.length;
         int colors = costs[0].length;
 
         int preMin = 0, preSecondMin = 0, preMinIdx = -1;
-        
-        for (int i = 0; i < houses; i++) {
+
+        for (int[] house : costs) {
             int curMin = Integer.MAX_VALUE, curSecondMin = Integer.MAX_VALUE, curMinIdx = -1;
             for (int j = 0; j < colors; j++) {
-                
-                int val = costs[i][j] + (j == preMinIdx ? preSecondMin : preMin);
-                
+
+                int val = house[j] + (j == preMinIdx ? preSecondMin : preMin);
+
                 if (curMinIdx == -1) {
                     curMin = val;
                     curMinIdx = j;
@@ -25,7 +24,7 @@ public class PaintHouseII {
                     curSecondMin = val;
                 }
             }
-           
+
             preMin = curMin;
             preSecondMin = curSecondMin;
             preMinIdx = curMinIdx;
