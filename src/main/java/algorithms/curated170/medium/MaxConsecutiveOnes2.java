47da5d3e3d3a43b9869130cee1af0f43f645d058
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-08 21:01:30 +0300

    487-Improve logic

--- src/main/java/algorithms/curated170/medium/MaxConsecutiveOnes2.java
+++ src/main/java/algorithms/curated170/medium/MaxConsecutiveOnes2.java
@@ -3,23 +3,21 @@ package algorithms.curated170.medium;
 public class MaxConsecutiveOnes2 {
 
     public int findMaxConsecutiveOnes(int[] nums) {
-        
+
         int best = 0;
         int leftCurr = 0, unitedCurr = 0;
         for (int n : nums) {
-            if(n == 1)
-            {
+            if (n == 1) {
                 leftCurr++;
                 unitedCurr++;
-            }
-            else
-            {
-                unitedCurr = leftCurr+1;
+            } else {
+                best = Math.max(best, unitedCurr);
+                unitedCurr = leftCurr + 1;
                 leftCurr = 0;
             }
-
-            best = Math.max(best, unitedCurr);
         }
+
+        best = Math.max(best, unitedCurr);
 
         return best;
     }
