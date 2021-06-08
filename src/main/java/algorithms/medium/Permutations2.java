bb0fb7852a9abc8ee51492ffaaafdddc58c903ab
altayhunoglu <muratfair@gmail.com>
2021-06-09 00:45:18 +0300

    Adds Backtracking solution: 47. Permutations II, moves Permutations to the right package.

---
+++ src/main/java/algorithms/medium/Permutations2.java
@@ -0,0 +1,44 @@
+package algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Permutations2 {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        backtrack(0, nums, output);
+        return output;
+
+    }
+    
+    public void backtrack(int choiceIndex, int[] nums, List<List<Integer>> output) {
+        if (choiceIndex == nums.length) {
+            ArrayList<Integer> permutation = new ArrayList<Integer>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            output.add(permutation);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        for (int i = choiceIndex; i < nums.length; i++) {
+            if (!visited.add(nums[i])) {
+                continue;
+            }
+            swap(choiceIndex, i, nums);
+            backtrack(choiceIndex + 1, nums, output);
+            swap(i, choiceIndex, nums);
+
+        }
+
+    }
+
+    public void swap(int choiceIndex, int i, int[] nums) {
+        int temp = nums[choiceIndex];
+        nums[choiceIndex] = nums[i];
+        nums[i] = temp;
+    }
+}
