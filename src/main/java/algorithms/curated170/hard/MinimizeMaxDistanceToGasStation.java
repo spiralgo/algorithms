ee036297a402ba292419327b71fdfbd6e015a807
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-09 22:17:25 +0300

    774-Add initial solution

---
+++ src/main/java/algorithms/curated170/hard/MinimizeMaxDistanceToGasStation.java
@@ -0,0 +1,37 @@
+package algorithms.curated170.hard;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+public class MinimizeMaxDistanceToGasStation {
+
+    public double solve(int[] stats, int k)
+    {
+        double left = 0, right = stats[stats.length-1];
+        while(left + 1e-6 < right)
+        {
+            double mid = (left+right)/2;
+            int count = 0;
+            for(int i = 0; i<stats.length-1; i++)
+            {
+                count += (int)((stats[i+1]-stats[i])/mid);
+            }
+            if(count > k)
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        var solution = new MinimizeMaxDistanceToGasStation();
+
+        System.out.println(solution.solve(new int[]{1,2,3,4,5},4));
+    }
+}
---
+++ src/main/java/algorithms/curated170/hard/MinimizeMaxDistanceToGasStationPriorityQueue.java
@@ -0,0 +1,28 @@
+package algorithms.curated170.hard;
+
+import java.util.PriorityQueue;
+
+public class MinimizeMaxDistanceToGasStationPriorityQueue {
+
+    public double solve(int[] stats, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a,b)->Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < stats.length - 1; i++) {
+            pq.offer(new double[] { stats[i + 1] - stats[i], 1 });
+        }
+        for(int i = 0; i<k; i++)
+        {
+            double[] pair = pq.poll();
+            pair[0] = pair[0] * pair[1] / (pair[1] + 1 );
+            pair[1]++;
+            pq.offer(pair);
+        }
+        return pq.poll()[0];
+    }
+
+    public static void main(String[] args) {
+        var solution = new MinimizeMaxDistanceToGasStationPriorityQueue();
+
+        System.out.println(solution.solve(new int[] { 1, 2, 3, 4, 5 }, 4));
+    }
+}
