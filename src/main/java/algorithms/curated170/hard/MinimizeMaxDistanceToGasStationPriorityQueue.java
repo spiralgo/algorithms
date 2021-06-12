94da2139da3fe89cfde760df232253528afee875
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-12 10:27:52 +0300

    774-Improve Priority Queue Solution

--- src/main/java/algorithms/curated170/hard/MinimizeMaxDistanceToGasStationPriorityQueue.java
+++ src/main/java/algorithms/curated170/hard/MinimizeMaxDistanceToGasStationPriorityQueue.java
@@ -1,25 +1,82 @@
 package algorithms.curated170.hard;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MinimizeMaxDistanceToGasStationPriorityQueue {
+    
+    PriorityQueue<Interval> pq;
 
-    public double minmaxGasDist(int[] stats, int k) {
-        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+    public double minmaxGasDist(int[] stations, int K) {
 
-        for (int i = 0; i < stats.length - 1; i++) {
-            pq.offer(new double[] { stats[i + 1] - stats[i], 1 });
-        }
+            pq = new PriorityQueue<Interval>(new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+
+                double diff = a.distance() - b.distance();
+                if (diff < 0) {
+                    return +1;
+                } else if (diff > 0) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        double leftToRight = stations[stations.length - 1] - stations[0];
         
-        for (int i = 0; i < k; i++) {
-            double[] pair = pq.poll();
+        int remaining = distributeStations(stations, K, leftToRight);
+        shareTheRest(remaining);
 
-            pair[0] = pair[0] * pair[1] / (++pair[1]);
+        Interval last = pq.poll();
+        return last.distance();
 
-            pq.offer(pair);
+    }
+
+    private void shareTheRest(int remaining) {
+        while (remaining > 0) {
+            Interval interval = pq.poll();
+            Interval nextLargest = pq.peek();
+            while(remaining > 0 && interval.distance() > nextLargest.distance())
+            {
+                interval.numInsertions++;
+                remaining--;
+            }
+            pq.add(interval);
+        }
+    }
+
+    private int distributeStations(int[] stations, int K, double leftToRight) {
+        int remaining = K;
+        for (int i = 0; i < stations.length - 1; i++) {
+            int numInsertions = (int) (K * calculateProportion(stations, i, leftToRight));
+
+            pq.add(new Interval(stations[i], stations[i + 1], numInsertions));
+            
+            remaining -= numInsertions;
+        }
+        return remaining;
+    }
+
+    private double calculateProportion(int[] stations, int i, double leftToRight)
+    {
+        return (double) (stations[i + 1] - stations[i]) / leftToRight;
+    }
+
+    class Interval {
+        double left;
+        double right;
+        int numInsertions;
+
+        double distance() {
+            return (right - left) / ((double) (numInsertions + 1));
         }
 
-        return pq.poll()[0];
+        Interval(double left, double right, int numInsertions) {
+            this.left = left;
+            this.right = right;
+            this.numInsertions = numInsertions;
+        }
     }
 
     public static void main(String[] args) {
