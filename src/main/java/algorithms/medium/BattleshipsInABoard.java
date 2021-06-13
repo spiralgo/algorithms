a82c314db0eb286ea1ec614906fb4a1110b05832
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-13 16:01:19 +0300

    419-Fix solution

--- src/main/java/algorithms/medium/BattleshipsInABoard.java
+++ src/main/java/algorithms/medium/BattleshipsInABoard.java
@@ -9,8 +9,8 @@ public class BattleshipsInABoard {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
 
-                if (board[i][j] == BATTLESHIP && hasBattleshipBefore(i, j, false, board)
-                        && hasBattleshipBefore(i, j, true, board)) {
+                if (board[i][j] == BATTLESHIP && !hasBattleshipBefore(i, j, false, board)
+                        && !hasBattleshipBefore(i, j, true, board)) {
                     count++;
                 }
 
@@ -21,9 +21,9 @@ public class BattleshipsInABoard {
 
     private boolean hasBattleshipBefore(int i, int j, boolean checkLeft, char[][] board) {
         if (checkLeft) {
-            return (j != 0 && board[i][j - 1] != 'X');
+            return (j != 0 && board[i][j - 1] == 'X');
         } else checkAbove: {
-            return (i != 0 && board[i - 1][j] != 'X');
+            return (i != 0 && board[i - 1][j] == 'X');
         }
     }
 
