faf1cbeeae5e363184473c456afc4b399246ec47
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-13 20:43:21 +0300

    251-Improve Solution 2

--- src/main/java/algorithms/curated170/medium/Flatten2DVector.java
+++ src/main/java/algorithms/curated170/medium/Flatten2DVector.java
@@ -3,44 +3,31 @@ package algorithms.curated170.medium;
 import java.util.NoSuchElementException;
 
 public class Flatten2DVector {
+
     class Vector2D {
 
         int[][] vec;
         int i = 0, j = 0;
 
-        public Vector2D(int[][] vec) {
-            this.vec = vec;
-            i = 0;
-            j = 0;
+        public Vector2D(int[][] v) {
+            this.vec = v;
         }
 
         public int next() {
-
             if (hasNext()) {
                 return vec[i][j++];
             } else {
                 throw new NoSuchElementException();
             }
-
-        }
-
-        private boolean checkArrays() {
-            j = 0;
-            do {
-                i++;
-                if (i >= vec.length) {
-                    return false;
-                }
-            } while (vec[i].length == 0);
-            return true;
         }
 
         public boolean hasNext() {
-            if (vec.length > 0 &&  j < vec[i].length) {
-                return true;
+            while (i < vec.length && j >= vec[i].length) {
+                i++;
+                j = 0;
             }
 
-            return checkArrays();
+            return i < vec.length;
         }
     }
 
