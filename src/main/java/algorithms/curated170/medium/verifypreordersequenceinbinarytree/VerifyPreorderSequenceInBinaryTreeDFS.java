d28cbdeea3fc1e5f6ba895d3ff9bcd5b1d9fc491
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-13 21:43:57 +0300

    255-Fix Small Changes

--- src/main/java/algorithms/curated170/medium/verifypreordersequenceinbinarytree/VerifyPreorderSequenceInBinaryTreeDFS.java
+++ src/main/java/algorithms/curated170/medium/verifypreordersequenceinbinarytree/VerifyPreorderSequenceInBinaryTreeDFS.java
@@ -5,15 +5,17 @@ import java.util.Stack;
 public class VerifyPreorderSequenceInBinaryTreeDFS {
 
     int i;
+
     public boolean verifyPreorder(int[] preorder) {
+        i = 0;
         DFS(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
         return i == preorder.length;
     }
 
-    private void DFS(int[] preorder,int lower, int upper )
-    {
-        if (i >= preorder.length || preorder[i] < lower || preorder[i] > upper)
-        return;
+    private void DFS(int[] preorder, int lower, int upper) {
+        if (i >= preorder.length || preorder[i] < lower || preorder[i] > upper) {
+            return;
+        }
 
         int val = preorder[i++];
         DFS(preorder, lower, val);
