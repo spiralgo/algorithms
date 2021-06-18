def37d7d3bfcd6884938e6b6196cb08862eb9f7f
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-18 10:53:57 +0300

    742-Change Map Keys

--- src/main/java/algorithms/curated170/medium/closestleafinabinarytree/ClosestLeafInABinaryTreeGraphBFS.java
+++ src/main/java/algorithms/curated170/medium/closestleafinabinarytree/ClosestLeafInABinaryTreeGraphBFS.java
@@ -11,72 +11,55 @@ import algorithms.datastructures.TreeNode;
 
 public class ClosestLeafInABinaryTreeGraphBFS {
 
-  Map<TreeNode, List<TreeNode>> graph;
-  int k;
-  TreeNode nodeK = null;
+  Map<Integer, List<TreeNode>> graph = new HashMap<>();
 
   public int findClosestLeaf(TreeNode root, int k) {
     if (root.left == null && root.right == null) {
       return k;
     }
 
-    this.k = k;
-    graph = new HashMap<>();
     createGraphWithDFS(root, null);
 
-    return shortestPathBFS(root);
+    return shortestPathBFS(root, k);
   }
 
-  private int shortestPathBFS(TreeNode root) {
-    Deque<TreeNode> queue = new ArrayDeque<>();
+  private int shortestPathBFS(TreeNode root, int k) {
+    Deque<Integer> queue = new ArrayDeque<>();
     boolean[] seen = new boolean[1001];
 
-    queue.offer(nodeK);
+    queue.offer(k);
     seen[k] = true;
 
     while (!queue.isEmpty()) {
-      TreeNode node = queue.poll();
-      if (node != null) {
-        if (graph.get(node).size() <= 1 && node != root) {
-          return node.val;
-        }
-        for (TreeNode adjacentNode : graph.get(node)) {
-          if (!seen[adjacentNode.val]) {
-            seen[adjacentNode.val] = true;
-            queue.add(adjacentNode);
-          }
+      Integer node = queue.poll();
+
+      List<TreeNode> children = graph.get(node);
+      if (node != root.val && children.size() <= 1) {
+        return node;
+      }
+      for (TreeNode adjacentNode : children) {
+        if (!seen[adjacentNode.val]) {
+          seen[adjacentNode.val] = true;
+          queue.add(adjacentNode.val);
         }
       }
+
     }
 
-    throw null;
+    return -1;
   }
 
   public void createGraphWithDFS(TreeNode node, TreeNode parent) {
     if (node != null) {
-      if (node.val == k) {
-        nodeK = node;
-      }
-      graph.put(node, new ArrayList<>());
+
+      graph.put(node.val, new ArrayList<>());
       if (parent != null) {
-        graph.get(node).add(parent);
-        graph.get(parent).add(node);
+        graph.get(node.val).add(parent);
+        graph.get(parent.val).add(node);
       }
 
       createGraphWithDFS(node.left, node);
       createGraphWithDFS(node.right, node);
     }
-  }
-
-  public static void main(String[] args) {
-    var solution = new ClosestLeafInABinaryTreeGraphBFS();
-
-    TreeNode tn1 = new TreeNode(1);
-    TreeNode t0 = new TreeNode(2, null, tn1);
-    TreeNode t1 = new TreeNode(5);
-    TreeNode t2 = new TreeNode(4, null, t1);
-    TreeNode t3 = new TreeNode(3, t0, t2);
-    TreeNode root = new TreeNode(9, null, t3);
-
   }
 }
