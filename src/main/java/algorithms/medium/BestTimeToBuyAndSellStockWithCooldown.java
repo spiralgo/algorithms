f5a77a4603069dad5431a1f5778bf11ae54af3d6
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-21 21:49:27 +0300

    309-Add Solution

---
+++ src/main/java/algorithms/medium/BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,25 @@
+package algorithms.medium;
+
+public class BestTimeToBuyAndSellStockWithCooldown {
+
+    public int maxProfit(int[] prices) {
+        
+        int maxProfit = 0, rest = 0, currBuyCost = Integer.MIN_VALUE;
+        for(int p : prices)
+        {
+            int prevBest = maxProfit;
+            maxProfit = Math.max(maxProfit, currBuyCost+p);
+            currBuyCost = Math.max(currBuyCost, rest-p);
+            rest = Math.max(rest, prevBest);
+        }
+        return maxProfit;
+    }
+
+	public static void main(String[] args) {
+		var solution = new BestTimeToBuyAndSellStockWithCooldown();
+		int[] prices = new int[] { 7, 1, 9, 3, 9, 4 };
+		System.out.println(solution.maxProfit(prices)); // prints 9, 3_9 transaction can't take place
+		prices = new int[] { 7, 6, 4, 3, 1 };
+		System.out.println(solution.maxProfit(prices)); // prints 0
+	}
+}
