177390c7e7d5549e3c646f5b7fa1d274476d9cd4
ErdemT09 <erdemturkoglu40@gmail.com>
2021-06-22 08:18:03 +0300

    309-Reformat Code

--- src/main/java/algorithms/medium/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/algorithms/medium/BestTimeToBuyAndSellStockWithCooldown.java
@@ -3,23 +3,22 @@ package algorithms.medium;
 public class BestTimeToBuyAndSellStockWithCooldown {
 
     public int maxProfit(int[] prices) {
-        
+
         int totalProfit = 0, restProfit = 0, currBuyCosts = Integer.MIN_VALUE;
-        for(int p : prices)
-        {
+        for (int p : prices) {
             int prevBest = totalProfit;
-            totalProfit = Math.max(totalProfit, currBuyCosts+p);
-            currBuyCosts = Math.max(currBuyCosts, restProfit-p);
+            totalProfit = Math.max(totalProfit, currBuyCosts + p);
+            currBuyCosts = Math.max(currBuyCosts, restProfit - p);
             restProfit = Math.max(restProfit, prevBest);
         }
         return totalProfit;
     }
 
-	public static void main(String[] args) {
-		var solution = new BestTimeToBuyAndSellStockWithCooldown();
-		int[] prices = new int[] { 7, 1, 9, 3, 9, 4 };
-		System.out.println(solution.maxProfit(prices)); // prints 9, 3_9 transaction can't take place
-		prices = new int[] { 7, 6, 4, 3, 1 };
-		System.out.println(solution.maxProfit(prices)); // prints 0
-	}
+    public static void main(String[] args) {
+        var solution = new BestTimeToBuyAndSellStockWithCooldown();
+        int[] prices = new int[] { 7, 1, 9, 3, 9, 4 };
+        System.out.println(solution.maxProfit(prices)); // prints 9, 3_9 transaction can't take place
+        prices = new int[] { 7, 6, 4, 3, 1 };
+        System.out.println(solution.maxProfit(prices)); // prints 0
+    }
 }
