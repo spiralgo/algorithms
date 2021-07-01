a62b5d60e1581dacd828e1c370b3d6499b249ba1
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-01 13:52:27 +0300

    333-Refactor Code

--- src/main/java/algorithms/curated170/medium/LargestBSTSubtree.java
+++ src/main/java/algorithms/curated170/medium/LargestBSTSubtree.java
@@ -9,19 +9,22 @@ public class LargestBSTSubtree {
     final int BST_SUBTREE_SIZE = 2;
 
     public int largestBSTSubtree(TreeNode root) {
-        return largestBST(root)[BST_SUBTREE_SIZE];
+        return getSizeOfLargestBST(root)[BST_SUBTREE_SIZE];
     }
 
-    private int[] largestBST(TreeNode node) {
+    private int[] getSizeOfLargestBST(TreeNode node) {
         if (node == null) {
             return new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE, 0 };
         }
-        int[] left = largestBST(node.left);
-        int[] right = largestBST(node.right);
+        int[] left = getSizeOfLargestBST(node.left);
+        int[] right = getSizeOfLargestBST(node.right);
+        
         if (isValidBST(node, left, right)) {
-            return new int[] { Math.min(node.val, left[MIN_VAL]), Math.max(node.val, right[MAX_VAL]), left[BST_SUBTREE_SIZE] + right[BST_SUBTREE_SIZE] + 1 };
+            return new int[] { Math.min(node.val, left[MIN_VAL]), Math.max(node.val, right[MAX_VAL]),
+                    left[BST_SUBTREE_SIZE] + right[BST_SUBTREE_SIZE] + 1 };
         } else {
-            return new int[] { Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left[BST_SUBTREE_SIZE], right[BST_SUBTREE_SIZE]) };
+            return new int[] { Integer.MIN_VALUE, Integer.MAX_VALUE,
+                    Math.max(left[BST_SUBTREE_SIZE], right[BST_SUBTREE_SIZE]) };
         }
     }
 
