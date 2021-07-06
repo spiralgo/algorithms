5ed4412701a2ee697e60f6142c1f18729c391478
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-06 13:48:11 +0300

    Quickselect-Fixes

--- src/main/java/algorithms/QuickSelect.java
+++ src/main/java/algorithms/QuickSelect.java
@@ -2,9 +2,9 @@ package algorithms;
 
 import java.util.Arrays;
 
-class Quickselect {
+public class Quickselect {
 
-  int select(int[] arr, int left, int right, int k) {
+  public int select(int[] arr, int left, int right, int k) {
     if (left == right) {
       return arr[left];
     }
@@ -19,7 +19,7 @@ class Quickselect {
     }
   }
 
-  int partition(int[] arr, int left, int right, int pivot) {
+  private int partition(int[] arr, int left, int right, int pivot) {
     int pivotVal = arr[pivot];
     swap(arr, pivot, right);
 
@@ -35,20 +35,28 @@ class Quickselect {
 
     return storeIndex;
   }
-  private int selectIterative(int[] nums, int start, int end, int k) {
+
+  public int selectIterative(int[] nums, int start, int end, int k) {
     // k is 1-indexed
-    int left = start, right = end, pivot = nums[(start+end)/2];
+    int left = start, right = end, pivot = nums[(start + end) / 2];
     while (left <= right) {
-        while (nums[left] < pivot) left++;
-        while (nums[right] > pivot) right--;
-        if (left >= right) break;
-        swap(nums, left++, right--);
+      while (nums[left] < pivot)
+        left++;
+      while (nums[right] > pivot)
+        right--;
+      if (left >= right)
+        break;
+      swap(nums, left++, right--);
     }
-    if (left-start >= k) return selectIterative(nums, start, left-1, k);
-    else if (left-start+1 == k && left == right) return nums[left];
-    else return selectIterative(nums, right+1, end, k-right+start-1);
-}
-  void swap(int[] arr, int i, int j) {
+    if (left - start >= k)
+      return selectIterative(nums, start, left - 1, k);
+    else if (left - start + 1 == k && left == right)
+      return nums[left];
+    else
+      return selectIterative(nums, right + 1, end, k - right + start - 1);
+  }
+
+  private void swap(int[] arr, int i, int j) {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
@@ -59,7 +67,7 @@ class Quickselect {
     var qs = new Quickselect();
     System.out.println(qs.select(arr, 0, arr.length - 1, 2)); // 3
     System.out.println(Arrays.toString(arr)); // the array is also partially or totally sorted
-    arr = new int[]{ 5, 3, 1, 2, 4 };
+    arr = new int[] { 5, 3, 1, 2, 4 };
     System.out.println(qs.selectIterative(arr, 0, arr.length - 1, 3)); // 3
   }
 }
