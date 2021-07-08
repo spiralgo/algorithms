90b3eeb1ff971d378a2f877896a1d0861e546fc0
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-08 08:12:09 +0300

    213-Add Check

--- src/main/java/algorithms/medium/HouseRobberII.java
+++ src/main/java/algorithms/medium/HouseRobberII.java
@@ -3,9 +3,13 @@ package algorithms.medium;
 public class HouseRobberII {
 
     public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
 
         int nowStolenA = 0, prevStolen = 0, stolen = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < len - 1; i++) {
             prevStolen = nowStolenA;
             nowStolenA = Math.max(prevStolen, stolen + nums[i]);
             stolen = prevStolen;
@@ -14,7 +18,7 @@ public class HouseRobberII {
         prevStolen = 0;
         stolen = 0;
         int nowStolenB = 0;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < len; i++) {
             prevStolen = nowStolenB;
             nowStolenB = Math.max(prevStolen, stolen + nums[i]);
             stolen = prevStolen;
