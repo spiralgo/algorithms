f73ec4cf0bf74cdab4d65e024dcf7b23eee80514
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-08 19:25:41 +0300

    269-Organize Imports

--- src/main/java/algorithms/curated170/hard/AlienDictionaryDFS.java
+++ src/main/java/algorithms/curated170/hard/AlienDictionaryDFS.java
@@ -1,8 +1,6 @@
 package algorithms.curated170.hard;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 public class AlienDictionaryDFS {
