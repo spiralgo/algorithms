a2a1ef748ab5f95a8ae65f27e0ae2e6156943eb9
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-09 11:21:09 +0300

    465-Update Solution

--- src/main/java/algorithms/curated170/hard/OptimalAccountBalancing.java
+++ src/main/java/algorithms/curated170/hard/OptimalAccountBalancing.java
@@ -1,86 +1,99 @@
 package algorithms.curated170.hard;
 
-public class OptimalAccountBalancing {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class OptimalAccountBalancing {
+    
     public int minTransfers(int[][] transactions) {
-        if (transactions == null || transactions.length == 0 || transactions[0].length == 0) {
-            return 0;
-        }
-        
         int n = transactions.length;
         if (n == 1) {
             return 1;
         }
-        
+
         int[] debt = buildDebtArray(transactions);
-        
-        int m = debt.length;
+
         Arrays.sort(debt);
-        int zeroNum = 0;
-        int res = 0;
+        
+        return cancelOppositeTransactions(debt);
+        
+    }
+    private int cancelOppositeTransactions(int[] debt)
+    {
+        int m = debt.length;
+        int cancelledCount = 0;
+        int transactions = 0;
         boolean[] isZero = new boolean[m];
+
         for (int i = 0, j = m - 1; i < j;) {
             int cur = debt[i] + debt[j];
+
             if (cur == 0) {
                 isZero[i++] = true;
-                isZero[j--] = true;                
-                res++;
-                zeroNum += 2;
+                isZero[j--] = true;
+                transactions++;
+                cancelledCount += 2;
             } else if (cur < 0) {
                 i++;
             } else {
                 j--;
             }
         }
-        int[] finalDebt = new int[m - zeroNum];
+
+        int[] finalDebt = new int[m - cancelledCount];
         for (int i = 0, j = 0; i < m; i++) {
             if (!isZero[i]) {
                 finalDebt[j++] = debt[i];
             }
         }
-        
-        return res + backtrack(0, finalDebt);
+
+        return transactions + tryHandlingRemainingTransactions(0, finalDebt);
     }
-    
+
     private int[] buildDebtArray(int[][] transactions) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int[] curTran: transactions) {
-            int from = curTran[0];
-            int to = curTran[1];
-            int amount = curTran[2];
-            map.put(from, map.getOrDefault(from, 0) - amount);
-            map.put(to, map.getOrDefault(to, 0) + amount);
+        Map<Integer, Integer> balances = new HashMap<>();
+        
+        for (int[] transaction : transactions) {
+            int from = transaction[0];
+            int to = transaction[1];
+            int amount = transaction[2];
+
+            balances.merge(from, -amount, Integer::sum);
+            balances.merge(to, +amount, Integer::sum);
         }
-        int[] res = new int[map.size()];
+
+        int[] debts = new int[balances.size()];
         int i = 0;
-        for (int x: map.values()) {
+
+        for (int x : balances.values()) {
             if (x != 0) {
-                res[i++] = x;
+                debts[i++] = x;
             }
         }
-        return Arrays.copyOf(res, i);
+
+        return Arrays.copyOf(debts, i);
     }
-    
-    private int backtrack(int curId, int[] debt) {
+
+    private int tryHandlingRemainingTransactions(int curId, int[] debt) {
         while (curId < debt.length && debt[curId] == 0) {
             curId++;
         }
-        // Base case.
+
         if (curId == debt.length) {
-            return 0;   
-        }            
-        // Recursive case.
-        int min = Integer.MAX_VALUE;
+            return 0;
+        }
+
+        int minTransactions = Integer.MAX_VALUE;
+
         for (int i = curId + 1; i < debt.length; i++) {
             if (debt[i] * debt[curId] < 0) {
                 debt[i] += debt[curId];
-                min = Math.min(min, 1 + backtrack(curId + 1, debt));
+                minTransactions = Math.min(minTransactions, 1 + tryHandlingRemainingTransactions(curId + 1, debt));
                 debt[i] -= debt[curId];
             }
         }
-        
-        return min;
+
+        return minTransactions;
     }
-
-
 }
