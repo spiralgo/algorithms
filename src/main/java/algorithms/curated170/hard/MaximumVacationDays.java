571255b0b8c6a0149be8f641851a240a5be2c917
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-20 20:59:55 +0300

    568-Refactor, Add DFS Solution

--- src/main/java/algorithms/curated170/hard/MaximumVacationDays.java
+++ src/main/java/algorithms/curated170/hard/MaximumVacationDays.java
@@ -10,6 +10,51 @@ public class MaximumVacationDays {
 
         int[] maxDays = new int[cities];
 
+        countDaysFirstWeek(flights, days, cities, maxDays);
+
+        for (int w = 1; w < weeks; w++) {
+            int[] nextWeekMaxes = new int[cities];
+
+            countStayingInCity(days, cities, maxDays, w, nextWeekMaxes);
+
+            countFlyingToCities(flights, days, cities, maxDays, w, nextWeekMaxes);
+
+            maxDays = nextWeekMaxes;
+        }
+
+        int maxVac = 0;
+        for (int vacDays : maxDays) {
+            maxVac = Math.max(maxVac, vacDays);
+        }
+
+        return maxVac;
+    }
+
+    private void countFlyingToCities(int[][] flights, int[][] days, int cities, int[] maxDays, int w,
+            int[] nextWeekMaxes) {
+        for (int from = 0; from < cities; from++) {
+            if (maxDays[from] == CANT_VISIT) {
+                continue;
+            }
+            for (int to = 0; to < cities; to++) {
+                if (flights[from][to] == 1) {
+                    nextWeekMaxes[to] = Math.max(nextWeekMaxes[to], maxDays[from] + days[to][w]);
+                }
+            }
+        }
+    }
+
+    private void countStayingInCity(int[][] days, int cities, int[] maxDays, int w, int[] nextWeekMaxes) {
+        for (int i = 0; i < cities; i++) {
+            if (maxDays[i] != CANT_VISIT) {
+                nextWeekMaxes[i] = maxDays[i] + days[i][w];
+            } else {
+                nextWeekMaxes[i] = CANT_VISIT;
+            }
+        }
+    }
+
+    private void countDaysFirstWeek(int[][] flights, int[][] days, int cities, int[] maxDays) {
         for (int c = 0; c < cities; c++) {
             if (flights[0][c] == 1) {
                 maxDays[c] = days[c][0];
@@ -18,38 +63,6 @@ public class MaximumVacationDays {
             }
         }
         maxDays[0] = days[0][0];
-
-        for (int w = 1; w < weeks; w++) {
-            int[] nextWeekMaxes = new int[cities];
-
-            for (int i = 0; i < cities; i++) {
-                if (maxDays[i] != CANT_VISIT) {
-                    nextWeekMaxes[i] = maxDays[i] + days[i][w];
-                } else {
-                    nextWeekMaxes[i] = CANT_VISIT;
-                }
-            }
-
-            for (int from = 0; from < cities; from++) {
-                if (maxDays[from] == CANT_VISIT) {
-                    continue;
-                }
-                for (int to = 0; to < cities; to++) {
-                    if (flights[from][to] == 1) {
-                        nextWeekMaxes[to] = Math.max(nextWeekMaxes[to], maxDays[from] + days[to][w]);
-                    }
-                }
-            }
-
-            maxDays = nextWeekMaxes;
-        }
-
-        int maxVacation = 0;
-        for (int vacDays : maxDays) {
-            maxVacation = Math.max(maxVacation, vacDays);
-        }
-
-        return maxVacation;
     }
 
     public static void main(String[] args) {
