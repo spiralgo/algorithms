f4330d9c8c98e7079a97edec627692ceb98da507
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-22 14:49:32 +0300

    1548-Refactor Variable

--- src/main/java/algorithms/curated170/hard/TheMostSimilarPathInAGraphQueue.java
+++ src/main/java/algorithms/curated170/hard/TheMostSimilarPathInAGraphQueue.java
@@ -33,28 +33,29 @@ public class TheMostSimilarPathInAGraphQueue {
         boolean[][] seen = new boolean[n][t];
         while (!paths.isEmpty()) {
             List<List<Integer>> path = paths.poll();
-            List<Integer> currCityDist = path.get(0);
-            List<Integer> currPath = path.get(1);
+            List<Integer> curCityAndDist = path.get(0);
+            List<Integer> currPathNodes = path.get(1);
 
-            int city = currCityDist.get(CITY), length = currCityDist.get(LENGTH);
+            int city = curCityAndDist.get(CITY), length = curCityAndDist.get(LENGTH);
             if (length == t - 1) {
-                return currPath;
+                return currPathNodes;
             }
 
-            List<Integer> nextPath;
+            List<Integer> nextPathNodes;
             for (int neighbor : graph[city]) {
                 if (seen[neighbor][length] == true) {
                     continue;
                 }
-
                 seen[neighbor][length] = true;
-                nextPath = new ArrayList<>(currPath);
-                nextPath.add(neighbor);
+
+                nextPathNodes = new ArrayList<>(currPathNodes);
+                nextPathNodes.add(neighbor);
+                List<List<Integer>> nextPath = List.of(List.of(neighbor, length + 1), nextPathNodes);
 
                 if (names[neighbor].equals(targetPath[length + 1])) {
-                    paths.addFirst(List.of(List.of(neighbor, length + 1), nextPath));
+                    paths.addFirst(nextPath);
                 } else {
-                    paths.addLast(List.of(List.of(neighbor, length + 1), nextPath));
+                    paths.addLast(nextPath);
                 }
             }
         }
@@ -86,9 +87,9 @@ public class TheMostSimilarPathInAGraphQueue {
             graph[i] = new int[adjacentCount[i]];
         }
         int[] currIdx = new int[n];
-        for (int[] edge : roads) {
-            graph[edge[0]][currIdx[edge[0]]++] = edge[1];
-            graph[edge[1]][currIdx[edge[1]]++] = edge[0];
+        for (int[] road : roads) {
+            graph[road[0]][currIdx[road[0]]++] = road[1];
+            graph[road[1]][currIdx[road[1]]++] = road[0];
         }
         return graph;
     }
