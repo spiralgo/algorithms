f0297b6807906b0aa4c30b71b8069f4281c4ab5d
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-23 16:28:24 +0300

    394-Refactor Method

--- src/main/java/algorithms/medium/DecodeString.java
+++ src/main/java/algorithms/medium/DecodeString.java
@@ -2,28 +2,41 @@ package algorithms.medium;
 
 public class DecodeString {
 
-    int index = 0;
+    int i = 0;
+    char[] data;
+    int len;
 
     String decodeString(String s) {
+        i = 0;
+        data = s.toCharArray();
+        len = data.length;
+        return buildDecodedStr().toString();
+    }
+
+    StringBuilder buildDecodedStr() {
         StringBuilder result = new StringBuilder();
-        while (index < s.length() && s.charAt(index) != ']') {
-            if (!Character.isDigit(s.charAt(index)))
-                result.append(s.charAt(index++));
+
+        while (i < len && data[i] != ']') {
+            if (!Character.isDigit(data[i])) {
+                result.append(data[i++]);
+            }
+
             else {
-                int k = 0;
-                // build k while next character is a digit
-                while (index < s.length() && Character.isDigit(s.charAt(index)))
-                    k = k * 10 + s.charAt(index++) - '0';
-                // ignore the opening bracket '['
-                index++;
-                String decodedString = decodeString(s);
-                // ignore the closing bracket ']'
-                index++;
-                // build k[decodedString] and append to the result
-                while (k-- > 0)
+                int reps = 0;
+
+                while (i < len && Character.isDigit(data[i])) {
+                    reps = reps * 10 + data[i++] - '0';
+                }
+
+                i++;
+                StringBuilder decodedString = buildDecodedStr();
+                i++;
+
+                while (reps-- > 0) {
                     result.append(decodedString);
+                }
             }
         }
-        return new String(result);
+        return result;
     }
 }
