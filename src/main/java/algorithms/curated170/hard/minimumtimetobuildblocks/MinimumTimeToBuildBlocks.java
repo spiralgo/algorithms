4cdefc385b32e5e2ca5f902fb84cf884edfff291
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-23 14:04:23 +0300

    1199-Reformat Solution

--- src/main/java/algorithms/curated170/hard/minimumtimetobuildblocks/MinimumTimeToBuildBlocks.java
+++ src/main/java/algorithms/curated170/hard/minimumtimetobuildblocks/MinimumTimeToBuildBlocks.java
@@ -3,15 +3,15 @@ package algorithms.curated170.hard;
 import java.util.PriorityQueue;
 
 public class MinimumTimeToBuildBlocks {
-    
+
     public int minBuildTime(int[] blocks, int split) {
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int b : blocks) pq.offer(b);
-        while(pq.size() > 1)
-        {
+        for (int b : blocks)
+            pq.offer(b);
+            
+        while (pq.size() > 1) {
             pq.poll();
-            pq.offer(pq.poll()+split);
+            pq.offer(pq.poll() + split);
         }
         return pq.poll();
     }
