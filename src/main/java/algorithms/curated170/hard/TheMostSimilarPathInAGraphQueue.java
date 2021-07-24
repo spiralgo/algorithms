6e45c7dfd4554d22763da87e80d598da860dacf5
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-24 12:07:35 +0300

    1548-Remove Elements to Class
    Co-Authored-By: altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>

--- src/main/java/algorithms/curated170/hard/TheMostSimilarPathInAGraphQueue.java
+++ src/main/java/algorithms/curated170/hard/TheMostSimilarPathInAGraphQueue.java
@@ -3,14 +3,10 @@ package algorithms.curated170.hard;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TheMostSimilarPathInAGraphQueue {
-
-    private static final int CITY = 0, LENGTH = 1;
-
+    
     public List<Integer> mostSimilar(int n, int[][] roads, String[] names, String[] targetPath) {
         int t = targetPath.length;
         if (t == 0) {
@@ -19,30 +15,30 @@ public class TheMostSimilarPathInAGraphQueue {
 
         int[][] graph = createGraph(n, roads);
 
-        Deque<List<List<Integer>>> paths = new ArrayDeque<>();
+        Deque<Path> paths = new ArrayDeque<>();
 
         setPathStarts(n, names, targetPath, paths);
 
         return buildPathWithQueue(graph, paths, names, targetPath, n);
     }
 
-    private List<Integer> buildPathWithQueue(int[][] graph, Deque<List<List<Integer>>> paths, String[] names, String[] targetPath,
+    private List<Integer> buildPathWithQueue(int[][] graph, Deque<Path> paths, String[] names, String[] targetPath,
             int n) {
         int t = targetPath.length;
 
         boolean[][] seen = new boolean[n][t];
         while (!paths.isEmpty()) {
-            List<List<Integer>> path = paths.poll();
-            List<Integer> curCityAndDist = path.get(0);
-            List<Integer> currPathNodes = path.get(1);
+            Path city = paths.poll();
+            List<Integer> currPathNodes = city.currPathNodes;
 
-            int city = curCityAndDist.get(CITY), length = curCityAndDist.get(LENGTH);
-            if (length == t - 1) {
+            int length = currPathNodes.size();
+
+            if (length == t) {
                 return currPathNodes;
             }
 
             List<Integer> nextPathNodes;
-            for (int neighbor : graph[city]) {
+            for (int neighbor : graph[city.cityIndex]) {
                 if (seen[neighbor][length] == true) {
                     continue;
                 }
@@ -50,9 +46,10 @@ public class TheMostSimilarPathInAGraphQueue {
 
                 nextPathNodes = new ArrayList<>(currPathNodes);
                 nextPathNodes.add(neighbor);
-                List<List<Integer>> nextPath = List.of(List.of(neighbor, length + 1), nextPathNodes);
 
-                if (names[neighbor].equals(targetPath[length + 1])) {
+                Path nextPath = new Path(neighbor, nextPathNodes);
+
+                if (names[neighbor].equals(targetPath[length])) {
                     paths.addFirst(nextPath);
                 } else {
                     paths.addLast(nextPath);
@@ -62,14 +59,15 @@ public class TheMostSimilarPathInAGraphQueue {
         return new ArrayList<>();
     }
 
-    private void setPathStarts(int n, String[] names, String[] targetPath, Deque<List<List<Integer>>> q) {
+    private void setPathStarts(int n, String[] names, String[] targetPath, Deque<Path> q) {
         for (int i = 0; i < n; i++) {
             List<Integer> list = new ArrayList<>();
             list.add(i);
+            Path initialCity = new Path(i, list);
             if (targetPath[0].equals(names[i])) {
-                q.addFirst(List.of(List.of(i, 0), list));
+                q.addFirst(initialCity);
             } else {
-                q.addLast(List.of(List.of(i, 0), list));
+                q.addLast(initialCity);
             }
         }
     }
@@ -92,6 +90,16 @@ public class TheMostSimilarPathInAGraphQueue {
             graph[road[1]][currIdx[road[1]]++] = road[0];
         }
         return graph;
+    }
+
+    private class Path {
+        Path(int cityIndex, List<Integer> currPathNodes) {
+            this.cityIndex = cityIndex;
+            this.currPathNodes = currPathNodes;
+        }
+
+        int cityIndex;
+        List<Integer> currPathNodes;
     }
 
 }
