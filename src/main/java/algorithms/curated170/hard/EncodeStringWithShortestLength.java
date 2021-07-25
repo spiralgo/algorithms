1ae9ced48c187bac14fad70745cda8586085d644
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-25 08:51:46 +0300

    471-Refactor 3

--- src/main/java/algorithms/curated170/hard/EncodeStringWithShortestLength.java
+++ src/main/java/algorithms/curated170/hard/EncodeStringWithShortestLength.java
@@ -3,43 +3,20 @@ package algorithms.curated170.hard;
 import java.util.HashMap;
 
 public class EncodeStringWithShortestLength {
-    HashMap<String, String> map;
+    HashMap<String, String> encodeMap;
 
     public String encode(String s) {
-        map = new HashMap<>();
+        encodeMap = new HashMap<>();
         return buildEncodedStr(s);
     }
 
     private String buildEncodedStr(String s) {
-        if (map.containsKey(s)) {
-            return map.get(s);
+        if (encodeMap.containsKey(s)) {
+            return encodeMap.get(s);
         }
         char[] data = s.toCharArray();
         int len = data.length;
-        String[][] ss = new String[len][len];
-
-        for (int i = 0; i < len; i++) {
-            int[] preStartIdx = new int[len - i + 1];
-            preStartIdx[0] = -1;
-            int repHeadLen = -1;
-
-            for (int j = 0; i + j < len; j++) {
-                repHeadLen = findRepetitionStart(data, i, preStartIdx, repHeadLen, j);
-                
-                int totalCodeLen = j + 1;
-                preStartIdx[totalCodeLen] = repHeadLen++;
-
-                int encodedStrLen = hasMultipleOfSubstr(repHeadLen, totalCodeLen) ? totalCodeLen - repHeadLen
-                        : totalCodeLen;
-                        
-                if (encodedStrLen == totalCodeLen) {
-                    ss[i][i + totalCodeLen - 1] = s.substring(i, i + totalCodeLen);
-                } else {
-                    String s1 = deliverEncodedStr(s, i, totalCodeLen, encodedStrLen);
-                    ss[i][i + j] = s1.length() < totalCodeLen ? s1 : s.substring(i, i + totalCodeLen);
-                }
-            }
-        }
+        String[][] ss = produce(s, data, len);
 
         int[] dp = new int[len + 1];
         int[] prev = new int[len + 1];
@@ -52,15 +29,44 @@ public class EncodeStringWithShortestLength {
                 }
             }
         }
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         int cur = len;
         while (cur > 0) {
             int p = prev[cur];
             sb.insert(0, ss[p][cur - 1]);
             cur = p;
         }
-        map.put(s, sb.toString());
+        encodeMap.put(s, sb.toString());
         return sb.toString();
+    }
+
+    private String[][] produce(String s, char[] data, int len) {
+        String[][] ss = new String[len][len];
+
+        for (int i = 0; i < len; i++) {
+            int[] preStartIdx = new int[len - i + 1];
+            preStartIdx[0] = -1;
+            int repHeadLen = -1;
+            for (int j = 0; j + i < len; j++) {
+                repHeadLen = findRepetitionStart(data, i, preStartIdx, repHeadLen, j);
+
+                int totalCodeLen = j + 1;
+                preStartIdx[totalCodeLen] = ++repHeadLen;
+
+                int encodedStrLen = hasMultipleOfSubstr(repHeadLen, totalCodeLen) ? totalCodeLen - repHeadLen
+                        : totalCodeLen;
+
+                if (encodedStrLen == totalCodeLen || totalCodeLen < 5) {
+                    ss[i][i + j] = s.substring(i, i + totalCodeLen);
+                } else {
+                    String encodedSubstr = deliverEncodedSubstr(s, i, totalCodeLen, encodedStrLen);
+                    ss[i][i + j] = encodedSubstr.length() < totalCodeLen ? encodedSubstr
+                            : s.substring(i, i + totalCodeLen);
+                }
+            }
+        }
+
+        return ss;
     }
 
     private int findRepetitionStart(char[] c, int i, int[] preStartIdx, int repHeadLen, int j) {
@@ -70,9 +76,9 @@ public class EncodeStringWithShortestLength {
         return repHeadLen;
     }
 
-    private String deliverEncodedStr(String s, int i, int totalCodeLen, int encodedStrLen) {
-        return String.valueOf(totalCodeLen / encodedStrLen) + "["
-                + buildEncodedStr(s.substring(i, i + encodedStrLen)) + "]";
+    private String deliverEncodedSubstr(String s, int i, int totalCodeLen, int encodedStrLen) {
+        return String.valueOf(totalCodeLen / encodedStrLen) + "[" + buildEncodedStr(s.substring(i, i + encodedStrLen))
+                + "]";
     }
 
     private boolean hasMultipleOfSubstr(int repHeadLen, int totalCodeLen) {
@@ -83,7 +89,7 @@ public class EncodeStringWithShortestLength {
         var solution = new EncodeStringWithShortestLength();
 
         System.out.println(solution.encode("abababfabababf"));
-        System.out.println(solution.map);
+        System.out.println(solution.encodeMap);
 
         /*
          * System.out.println(solution.encode("abcdabcd"));
