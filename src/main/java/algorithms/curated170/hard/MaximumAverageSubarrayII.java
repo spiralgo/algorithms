fdc058571de3e5659b288db79e549f095790c9f6
ErdemT09 <63192680+ErdemT09@users.noreply.github.com>
2021-07-25 09:17:34 +0300

    644-Modularize Method
    Co-authored-by: altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>

--- src/main/java/algorithms/curated170/hard/MaximumAverageSubarrayII.java
+++ src/main/java/algorithms/curated170/hard/MaximumAverageSubarrayII.java
@@ -22,15 +22,23 @@ public class MaximumAverageSubarrayII {
         return lo;
     }
     
-    public boolean canFind(int[] sums, int k, double v) {
+          public boolean canFind(int[] sums, int k, double mid) {
         double sum = 0;
-        double minPreSum = 0;
+        double preSum=0, minPreSum = 0;
         for (int i = k-1; i < sums.length; i++) {
-            sum = sums[i] - (i+1) * v;
-            if (i-k >= 0) minPreSum = Math.min(minPreSum, sums[i-k] - (i-k+1) * v);
-            if (sum - minPreSum > 0) return true;
+            sum = getSumInRange(i,0, sums, mid);
+           
+            if (i-k >= 0){
+                 preSum = getSumInRange(i,k, sums, mid);
+                 minPreSum = Math.min(minPreSum, preSum);
+            } 
+            if (sum - minPreSum >= 0) return true;
         }
         return false;
     }
+    
+     double getSumInRange(int i, int k, int[] sums, double mid){
+        return sums[i-k] - (i-k+1) * mid;
+     }
 
 }
