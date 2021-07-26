cb687d0885ea70c4ec39bf0a793268eb6a1deba3
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-26 18:22:26 +0300

    317-Add BFS Solution

--- src/main/java/algorithms/curated170/hard/ShortestDistanceFromAllBuildings.java
+++ src/main/java/algorithms/curated170/hard/ShortestDistanceFromAllBuildings.java
@@ -1,53 +1,73 @@
 package algorithms.curated170.hard;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ShortestDistanceFromAllBuildings {
 
-    int m, n;
-    int[][] grid;
-    int[][] count;
+    private static int[] dir = { -1, 0, 1, 0, -1 };
+
+    private int m, n;
 
     public int shortestDistance(int[][] grid) {
+
         m = grid.length;
         n = grid[0].length;
-        this.grid = grid;
-        count = new int[m][n];
 
-        for (int i = 0; i < m; i++) {
+        int visitCount = 0;
+        int minDist = 0;
+
+        int[][] dist = new int[m][n];
+
+        for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    BFS(i, j, new int[m][n], 0);
+                    minDist = helper(grid, i, j, visitCount, dist);
+                    if (minDist == Integer.MAX_VALUE)
+                        return -1;
+                    visitCount++;
+                }
+            }
+
+        return minDist;
+    }
+
+    private int helper(int[][] grid, int i, int j, int visitCount, int[][] dist) {
+
+        int minDist = Integer.MAX_VALUE;
+
+        Deque<int[]> tiles = new ArrayDeque<>();
+        tiles.add(new int[] { i, j });
+        int layer = 0;
+
+        while (!tiles.isEmpty()) {
+            int levelSize = tiles.size();
+
+            layer++;
+            while (levelSize --> 0) {
+                int[] t = tiles.poll();
+
+                for (int k = 0; k < 4; k++) {
+                    int nx = t[0] + dir[k];
+                    int ny = t[1] + dir[k + 1];
+                    if (!outOfRangeOrVisited(nx, ny, grid, visitCount)) {
+                        dist[nx][ny] += layer;
+                        grid[nx][ny]--;
+                        tiles.add(new int[] { nx, ny });
+
+                        minDist = Math.min(minDist, dist[nx][ny]);
+                    }
                 }
             }
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0)
-                    min = Math.min(min, count[i][j]);
-            }
-        }
 
-        return min;
+        return minDist;
     }
 
-    void BFS(int a, int b, int[][] dist, int total) {
-        if (noVisit(a, b, dist, total)) {
-            return;
-        }
-        count[a][b] -= dist[a][b];
-        count[a][b] += (dist[a][b] = total);
+    private static final int INVERT_NEGATIVE_COUNT = -1;
 
-        BFS(a + 1, b, dist, total + 1);
-        BFS(a - 1, b, dist, total + 1);
-        BFS(a, b + 1, dist, total + 1);
-        BFS(a, b - 1, dist, total + 1);
-    }
-
-    private boolean noVisit(int a, int b, int[][] dist, int total) {
-        return a == m || a == -1 || b == -1 || b == n || grid[a][b] != 0 || (dist[a][b] != 0 &&  dist[a][b] < total);
+    private boolean outOfRangeOrVisited(int nx, int ny, int[][] grid, int visitCount) {
+        return nx == m || nx == -1 || ny == -1 || ny == n || INVERT_NEGATIVE_COUNT * grid[nx][ny] != visitCount;
     }
 
     public static void main(String[] args) {
