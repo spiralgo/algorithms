0d8419253e22ceb7932a4541f98676b28ee7fadf
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-26 11:51:05 +0300

    471-Refactor DP Solution

--- src/main/java/algorithms/curated170/hard/EncodeStringWithShortestLengthDP.java
+++ src/main/java/algorithms/curated170/hard/EncodeStringWithShortestLengthDP.java
@@ -1,40 +1,53 @@
 package algorithms.curated170.hard;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-
 public class EncodeStringWithShortestLengthDP {
-    
+
     public String encode(String s) {
         int n = s.length();
-        String[][] dp = new String[n][n];
-        for (int j = 0; j < n; ++j) {
+        String[][] codings = new String[n][n];
+        for (int e = 0; e < n; e++) {
 
-            dp[j][j] = s.substring(j, j + 1);
-            for (int p = 0; p < j; p++) {
-                dp[p][j] = dp[p][j - 1] + dp[j][j];
-            }
+            initializeEndings(s, codings, e);
 
-            for (int i = j - 1; 2*i >= j - 1; --i) {
-                String sub = s.substring(i + 1, j + 1); // s[i+1..j]
-                for (int k = 2*i - (j - 1); k >= 0 && sub.equals(s.substring(k, k + j - i)); k -= j - i) {
-                    String str = String.valueOf((j + 1 - k) / (j - i)) + "[" + dp[i + 1][j] + "]";
-                    if (str.length() < dp[k][j].length()) {
-                        dp[k][j] = str;
-                        
-                        for (int p = 0; p < k; ++p) {
-                            if (dp[p][k - 1].length() + str.length() < dp[p][j].length()) {
-                                dp[p][j] = dp[p][k - 1] + str;
-                            }
-                        }
-                        
-                    }
+            for (int b = e - 1; 2 * b >= e - 1; --b) {
+                String sub = s.substring(b + 1, e + 1);
+                int encodedStrLen = e - b;
+                
+                for (int repStart = 2 * b - (e - 1); repStart >= 0 && sub.equals(s.substring(repStart, repStart + encodedStrLen)); repStart -= encodedStrLen) {
+
+                    String str = String.valueOf((e - repStart + 1) / encodedStrLen) + "[" + codings[b + 1][e] + "]";
+                    
+                    updatePreviousCodings(codings, e, repStart, str);
                 }
             }
         }
-        return dp[0][n - 1];
+        
+        return codings[0][n - 1];
     }
 
+    private void initializeEndings(String s, String[][] codings, int e) {
+        codings[e][e] = s.substring(e, e + 1);
+        for (int i = 0; i < e; i++) {
+            codings[i][e] = codings[i][e - 1] + codings[e][e];
+        }
+    }
+
+    private void updatePreviousCodings(String[][] codings, int e, int repStart, String str) {
+        if (str.length() < codings[repStart][e].length()) {
+            codings[repStart][e] = str;
+
+            for (int u = 0; u < repStart; u++) {
+                if (codings[u][repStart - 1].length() + str.length() < codings[u][e].length()) {
+                    codings[u][e] = codings[u][repStart - 1] + str;
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        var solution = new EncodeStringWithShortestLengthDP();
+
+        System.out.println(solution.encode("abababab"));
+    }
 }
