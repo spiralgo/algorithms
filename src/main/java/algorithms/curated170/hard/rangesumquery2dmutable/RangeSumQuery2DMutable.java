f7b820eaaedd10b3a15a0223327af02abcdda40c
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-28 15:17:50 +0300

    308-Add Initial Solution

--- src/main/java/algorithms/curated170/hard/rangesumquery2dmutable/RangeSumQuery2DMutable.java
+++ src/main/java/algorithms/curated170/hard/rangesumquery2dmutable/RangeSumQuery2DMutable.java
@@ -2,10 +2,43 @@ package algorithms.curated170.hard.rangesumquery2dmutable;
 
 public class RangeSumQuery2DMutable {
 
+    class NumMatrix {
+        
+        private int[][] colSums;
+        private int[][] matrix;
 
-    
+        public NumMatrix(int[][] matrix) {
+            this.matrix = matrix;
+            
+            int m = matrix.length;
+            int n = matrix[0].length;
+            colSums = new int[m + 1][n];
+            
+            for (int i = 1; i <= m; i++) {
+                for (int j = 0; j < n; j++) {
+                    colSums[i][j] = colSums[i - 1][j] + matrix[i - 1][j];
+                }
+            }
+        }
 
-    public static void main(String[] args) {
+        public void update(int row, int col, int val) {
+            for (int i = row + 1; i < colSums.length; i++) {
+                colSums[i][col] += (val - matrix[row][col]);
+            }
 
+            matrix[row][col] = val;
+        }
+
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            int sum = 0;
+
+            for (int j = col1; j <= col2; j++) {
+                sum += colSums[row2 + 1][j] - colSums[row1][j];
+            }
+
+            return sum;
+        }
     }
+
 }
