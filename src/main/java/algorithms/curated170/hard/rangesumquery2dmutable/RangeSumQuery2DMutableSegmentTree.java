4c7e6046b28966de4fa6ae3aaa298cfac02a1101
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-29 08:32:22 +0300

    308-Improve Segment Tree

--- src/main/java/algorithms/curated170/hard/rangesumquery2dmutable/RangeSumQuery2DMutableSegmentTree.java
+++ src/main/java/algorithms/curated170/hard/rangesumquery2dmutable/RangeSumQuery2DMutableSegmentTree.java
@@ -1,78 +1,90 @@
 package algorithms.curated170.hard.rangesumquery2dmutable;
 
+import java.util.Arrays;
+
 public class RangeSumQuery2DMutableSegmentTree {
 
     class NumMatrix {
-        final int M, N;
-        int[][] segTree;
-        int[][] _matrix;
+        int[][] tree;
+        int m, n;
+
         public NumMatrix(int[][] matrix) {
-            M=matrix.length;
-            N=matrix[0].length;
-            this._matrix=matrix;
-            
-            segTree=new int[M][4*N];    
-            for(int i=0;i<M;i++){
-                insert(i,matrix[i],0,0,N-1);
+            m = matrix.length;
+            n = matrix[0].length;
+            tree = new int[m << 1][n << 1];
+            buildTree(matrix);
+        }
+
+        private void buildTree(int[][] mat) {
+            for (int i = m; i < (m << 1); i++) {
+                for (int j = n; j < (n << 1); j++) {
+                    tree[i][j] = mat[i - m][j - n];
+                }
+            }
+            for (int i = m - 1; i > 0; i--) {
+                for (int j = n; j < (n << 1); j++) {
+                    tree[i][j] = tree[2 * i][j] + tree[2 * i + 1][j];
+                }
+            }
+
+            for (int i = 1; i < (m << 1); i++) {
+                for (int j = n - 1; j > 0; j--) {
+                    tree[i][j] = tree[i][2 * j] + tree[i][2 * j + 1];
+                }
             }
         }
-        
-        private void insert(int segTreeNum, int[] arr,int treeIdx,int lo, int hi){
-            if(lo==hi){
-                segTree[segTreeNum][treeIdx]=arr[lo];
-                return;
-            }
-            int mid=lo+(hi-lo)/2;
-            insert(segTreeNum,arr,2*treeIdx+1,lo,mid);    
-            insert(segTreeNum,arr,2*treeIdx+2,mid+1,hi);
-            
-            segTree[segTreeNum][treeIdx]=segTree[segTreeNum][2*treeIdx+1]+segTree[segTreeNum][2*treeIdx+2];
-        }
-        
-        
-        private void update(int segTreeNum,int arrIdx,int treeIdx, int lo, int hi ){
-            if(lo==hi){
-                segTree[segTreeNum][treeIdx]=_matrix[segTreeNum][arrIdx];
-                return;
-            }
-            int mid=lo+(hi-lo)/2;
-            if(arrIdx<=mid){
-                update(segTreeNum,arrIdx,2*treeIdx+1,lo,mid);    
-            }else{
-                update(segTreeNum,arrIdx,2*treeIdx+2,mid+1,hi);
-            }
-            segTree[segTreeNum][treeIdx]=segTree[segTreeNum][2*treeIdx+1]+segTree[segTreeNum][2*treeIdx+2];
-        }
-        
+
         public void update(int row, int col, int val) {
-            _matrix[row][col]=val;
-            update(row,col,0,0,N-1);
-        }
-        
-        public int sumRegion(int row1, int col1, int row2, int col2) {
-            int ans=0;
-            for(int r = row1; r<=row2; r++){
-                ans+=query(r,0,0,N-1,col1,col2);
+            row += m;
+            col += n;
+
+            int i = row, j = col;
+            for (tree[i][j] = val; j > 0; j >>= 1) {
+                tree[i][j >> 1] = tree[i][j] + tree[i][j ^ 1];
             }
-            return ans;
-        }
-        
-        private int query(int segTreeNum,int treeIdx, int lo, int hi, int ql, int qr){
-            if(lo>qr||hi<ql){return 0;}
-            if(ql<=lo && qr>=hi){return segTree[segTreeNum][treeIdx];}
-            
-            int mid = lo+(hi-lo)/2;
-            
-            if(qr<=mid){
-                return query(segTreeNum,2*treeIdx+1,lo,mid,ql,qr); 
-            }else if(ql>mid){
-                return query(segTreeNum,2*treeIdx+2,mid+1,hi,ql,qr);
+
+            for (i = row >> 1; i > 0; i >>= 1) {
+                tree[i][col] = tree[i << 1 | 1][col] + tree[i << 1][col];
+                for (j = col; j > 0; j >>= 1) {
+                    tree[i][j >> 1] = tree[i][j] + tree[i][j ^ 1];
+                }
             }
-            
-            int left=query(segTreeNum,2*treeIdx+1,lo,mid,ql,mid);
-            int right=query(segTreeNum,2*treeIdx+2,mid+1,hi,mid+1,qr);
-            
-            return left+right;
         }
+
+        public int sumRegion(int row1, int c1, int row2, int c2) {
+            int sum = 0;
+            for (row1 += m, row2 += m; row1 <= row2; row1 >>= 1, row2 >>= 1) {
+                if ((row1 & 1) == 1) {
+                    sum += sumRange(row1, c1, c2);
+                    row1++;
+                }
+                if ((row2 & 1) == 0) {
+                    sum += sumRange(row2, c1, c2);
+                    row2--;
+                }
+            }
+            return sum;
+        }
+
+        private int sumRange(int r, int c1, int c2) {
+            int sum = 0;
+            for (c1 += n, c2 += n; c1 <= c2; c1 >>= 1, c2 >>= 1) {
+                if ((c1 & 1) == 1)
+                    sum += tree[r][c1++];
+                if ((c2 & 1) == 0)
+                    sum += tree[r][c2--];
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        var solution = new RangeSumQuery2DMutableSegmentTree();
+
+        int[][] matrix = new int[][] { { 4, 7, 3, -1 }, { 2, 1, 9, 5 }, { -7, 3, 4, 0 }, { 8, 2, 1, 6 } };
+        var nm = solution.new NumMatrix(matrix);
+
+        nm.update(2, 1, 7);
+        System.out.println(nm.sumRegion(1, 1, 3, 2));
     }
 }
