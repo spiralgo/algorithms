e173ca05389c42bf6e113c489315f6857acbeb69
ErdemT09 <erdemturkoglu40@gmail.com>
2021-07-31 21:26:49 +0300

    428-Add Nested Solution

---
+++ src/main/java/algorithms/curated170/hard/SerializeAndDeserializeNaryTreeNestedList.java
@@ -0,0 +1,91 @@
+package algorithms.curated170.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class SerializeAndDeserializeNaryTreeNestedList {
+
+    static class Codec {
+
+        public String serialize(Node root) {
+            if (root == null) {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(root.val);
+            sb.append("[");
+            for (Node child : root.children) {
+                sb.append(serialize(child));
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+        public Node deserialize(String data) {
+            Node root = null;
+            Stack<Node> stack = new Stack<>();
+            int i = 0;
+
+            while (i < data.length()) {
+                int start = i;
+
+                // Move pointer forward until we don't find a digit...
+                while (i < data.length() && Character.isDigit(data.charAt(i))) {
+                    i++;
+                }
+
+                // If we haven't found a digit then we must have found the end of a child
+                // list...
+                if (start == i) {
+                    Node child = stack.pop();
+                    if (stack.isEmpty()) {
+                        root = child;
+                        break;
+                    } else {
+                        // Remove the child from the stack and assign it to the previous node on the
+                        // stack
+                        stack.peek().children.add(child);
+                    }
+                } else {
+                    Node n = new Node();
+                    n.val = Integer.parseInt(data.substring(start, i));
+                    n.children = new ArrayList<>();
+                    stack.push(n);
+                }
+                i++;
+            }
+            return root;
+        }
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> children = new ArrayList<>();
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node c1 = new Node(10);
+        Node c2 = new Node(77);
+        Node c3 = new Node(24);
+        Node root = new Node(90, List.of(c1, c2, c3));
+
+        var codec = new Codec();
+        String coded = codec.serialize(root);
+        System.out.println(coded);
+        System.out.println(codec.serialize(codec.deserialize(coded)));
+    }
+}
