dbebf182c0f9b98d3bb2bcf7b7c2259f058d2621
ErdemT09 <erdemturkoglu40@gmail.com>
2021-08-02 09:55:17 +0300

    Patch 362.1

--- src/main/java/algorithms/curated170/medium/designahitcounter/DesignAHitCounter.java
+++ src/main/java/algorithms/curated170/medium/designahitcounter/DesignAHitCounter.java
@@ -1,13 +1,15 @@
 package algorithms.curated170.medium.designahitcounter;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 
 public class DesignAHitCounter {
-    public static class HitCounter {
-        private List<Integer> hits = new LinkedList<>();
+    public class HitCounter {
+
+        private Queue<Integer> hits;
 
         public HitCounter() {
+            hits = new LinkedList<>();
         }
 
         public void hit(int time) {
@@ -15,49 +17,17 @@ public class DesignAHitCounter {
         }
 
         public int getHits(int timestamp) {
-            int size = hits.size();   
-            int l = timestamp <= 300 ? 0 : timestamp - 300;
-    
-            int index = 0;
-            for (int i: hits) {
-                if (i <= l) {
-                    index++;
-                }else break;
+            while (!hits.isEmpty() && timestamp - hits.peek() >= 300) {
+                hits.poll();
             }
-            
-            return size - index;
-        }
-
-        //Another way of solving:
-        public int getHitsWithBinarySearch(int time) {
-            adjustValidHits(time);
             return hits.size();
-        }
-
-        private void adjustValidHits(int time) {
-            int size = hits.size();
-            if (size==0 || time - hits.get(0) < 300 ) {
-                return;
-            }
-            if (time - hits.get(size - 1) >= 300) {
-                hits.clear();
-                return;
-            }
-            int l = 0, r = size;
-            while (r >= l) {
-                int mid = (l + r) / 2;
-                if (time - hits.get(mid) >= 300) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            }
-            hits = hits.subList(l, size);
         }
     }
 
     public static void main(String[] args) {
-        var hitCounter = new HitCounter();
+        var solution = new DesignAHitCounter();
+        var hitCounter = solution.new HitCounter();
+
         hitCounter.hit(1);
         hitCounter.hit(2);
         hitCounter.hit(3);
@@ -67,7 +37,7 @@ public class DesignAHitCounter {
         hitCounter.hit(13);
         hitCounter.hit(13);
         hitCounter.hit(302);
-        System.out.println(hitCounter.getHits(301)); //prints 8
-        System.out.println(hitCounter.getHits(305)); //prints 6
+        System.out.println(hitCounter.getHits(301)); // prints 8
+        System.out.println(hitCounter.getHits(305)); // prints 6
     }
 }
