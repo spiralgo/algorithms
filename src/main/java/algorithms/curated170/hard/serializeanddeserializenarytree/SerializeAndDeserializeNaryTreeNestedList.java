c2264db54be3145911f02b52a38561af8ab0145f
ErdemT09 <63192680+ErdemT09@users.noreply.github.com>
2021-08-03 13:18:33 +0300

    Update src/main/java/algorithms/curated170/hard/serializeanddeserializenarytree/SerializeAndDeserializeNaryTreeNestedList.java
    Co-authored-by: altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>

--- src/main/java/algorithms/curated170/hard/serializeanddeserializenarytree/SerializeAndDeserializeNaryTreeNestedList.java
+++ src/main/java/algorithms/curated170/hard/serializeanddeserializenarytree/SerializeAndDeserializeNaryTreeNestedList.java
@@ -56,22 +56,7 @@ public class SerializeAndDeserializeNaryTreeNestedList {
         }
     }
 
-    static class Node {
-        public int val;
-        public List<Node> children = new ArrayList<>();
-
-        public Node() {
-        }
-
-        public Node(int _val) {
-            val = _val;
-        }
-
-        public Node(int _val, List<Node> _children) {
-            val = _val;
-            children = _children;
-        }
-    }
+ 
 
     public static void main(String[] args) {
         Node c1 = new Node(10);
