8aaeb142c654f22f362816a1a68d32dc971a584a
ErdemT09 <erdemturkoglu40@gmail.com>
2021-08-03 13:41:04 +0300

    Patch 1538.1

--- src/main/java/algorithms/curated170/medium/GuessTheMajorityInAHiddenArray.java
+++ src/main/java/algorithms/curated170/medium/GuessTheMajorityInAHiddenArray.java
@@ -1,19 +1,12 @@
 package algorithms.curated170.medium;
 
  public class GuessTheMajorityInAHiddenArray {
+    
     public int guessMajority(ArrayReader reader) {
         int n = reader.length();
         boolean[] val = new boolean[5];
-        int a = reader.query(0, 1, 2, 3);
-        int b = reader.query(0, 2, 3, 4);
-        int c = reader.query(0, 1, 3, 4);
-        int d = reader.query(0, 1, 2, 4);
-        int e = reader.query(1, 2, 3, 4);
-        val[0] = true;
-        val[1] = (b == e);
-        val[2] = (b == c) ? val[1] : !val[1];
-        val[3] = (c == d) ? val[2] : !val[2];
-        val[4] = (a == d) ? val[3] : !val[3];
+        int secondFourWindowCount = compareFirstFiveValues(reader, val);
+
         int countA = 0, countB = 0, indexA = -1, indexB = -1;
         for (int i = 0; i < 5; i++) {
             if (val[i]) {
@@ -24,11 +17,34 @@ package algorithms.curated170.medium;
                 indexB = i;
             }
         }
-        int prev = e;
+
+        return compareNextWindows(reader, n, val, secondFourWindowCount, countA, countB, indexA, indexB);
+    }
+
+    private int compareFirstFiveValues(ArrayReader reader, boolean[] val) {
+        int a = reader.query(0, 1, 2, 3);
+        int b = reader.query(0, 2, 3, 4);
+        int c = reader.query(0, 1, 3, 4);
+        int d = reader.query(0, 1, 2, 4);
+        int e = reader.query(1, 2, 3, 4);
+        val[0] = true;
+        val[1] = (b == e);
+        val[2] = (b == c) ? val[1] : !val[1];
+        val[3] = (c == d) ? val[2] : !val[2];
+        val[4] = (a == d) ? val[3] : !val[3];
+        return e;
+    }
+
+    private int compareNextWindows(ArrayReader reader, int n, boolean[] val, int secondFourWindowCount, int countA, int countB,
+            int indexA, int indexB) {
+
+        int prevWin = secondFourWindowCount;
+
         for (int i = 5; i < n; i++) {
-            int cur = reader.query(i - 3, i - 2, i - 1, i);
-            val[i % 5] = (cur == prev) ? val[(i - 4) % 5] : !val[(i - 4) % 5];
-            prev = cur;
+            int curWin = reader.query(i - 3, i - 2, i - 1, i);
+
+            val[i % 5] = compareToMissingVal(val, prevWin, i, curWin);
+            
             if (val[i % 5]) {
                 countA++;
                 indexA = i;
@@ -36,12 +52,21 @@ package algorithms.curated170.medium;
                 countB++;
                 indexB = i;
             }
+
+            prevWin = curWin;
         }
+        
         if (countA == countB) {
             return -1;
         }
         return countA > countB ? indexA : indexB;
     }
+
+    private boolean compareToMissingVal(boolean[] val, int prevWin, int i, int curWin) {
+        return (curWin == prevWin) ? val[(i - 4) % 5] : !val[(i - 4) % 5];
+    }
+
+
 
     public static void main(String[] args) {
 
