b89656ee7010e06b1bc80aebfe3264fe1073d19f
ErdemT09 <erdemturkoglu40@gmail.com>
2021-08-08 08:50:35 +0300

    Patch 1427.1

--- src/main/java/algorithms/curated170/easy/PerformStringShifts.java
+++ src/main/java/algorithms/curated170/easy/PerformStringShifts.java
@@ -1,19 +1,30 @@
 package algorithms.curated170.easy;
 
 public class PerformStringShifts {
-   public String stringShift(String string, int[][] shift) {
+  public String stringShift(String string, int[][] shift) {
+
+    int[] overallShifts = new int[2];
+    countLeftRightShifts(shift, overallShifts);
+
+    int leftShifts = overallShifts[0];
+    int rightShifts = overallShifts[1];
+
     int len = string.length();
-    for (int[] move : shift) {
-      int direction = move[0];
-      int amount = move[1] % len;
-      if (direction == 0) {
-        // Move necessary amount of characters from front to end 
-        string = string.substring(amount) + string.substring(0, amount);
-      } else {
-        // Move necessary amount of characters from end to front
-        string = string.substring(len - amount) + string.substring(0, len - amount);
-      }
+    if (leftShifts > rightShifts) {
+      leftShifts = (leftShifts - rightShifts) % len;
+      string = string.substring(leftShifts) + string.substring(0, leftShifts);
+    } else if (rightShifts > leftShifts) {
+      rightShifts = (rightShifts - leftShifts) % len;
+      string = string.substring(len - rightShifts) + string.substring(0, len - rightShifts);
     }
+
     return string;
   }
+
+  private void countLeftRightShifts(int[][] shift, int[] overallShifts) {
+    for (int[] move : shift) {
+      overallShifts[move[0]] += move[1];
+    }
+  }
+  
 }
