a1549e107355593a27e985181dea82e4e7119c44
altay.hunoglu <altay.hunoglu@solvians.com>
2024-02-07 23:55:10 +0100

    Adds samples to test it with main method.

--- src/main/java/algorithms/curated170/easy/MeetingRooms.java
+++ src/main/java/algorithms/curated170/easy/MeetingRooms.java
@@ -12,4 +12,24 @@ public class MeetingRooms {
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        MeetingRooms solution = new MeetingRooms();
+
+        // Sample 1: Meetings do not overlap
+        int[][] intervals1 = {{0, 30}, {35, 40}, {45, 50}};
+        System.out.println("Can attend all meetings? (Sample 1): " + solution.canAttendMeetings(intervals1));
+
+        // Sample 2: Meetings overlap
+        int[][] intervals2 = {{0, 30}, {25, 35}, {45, 50}};
+        System.out.println("Can attend all meetings? (Sample 2): " + solution.canAttendMeetings(intervals2));
+
+        // Sample 3: No meetings
+        int[][] intervals3 = {};
+        System.out.println("Can attend all meetings? (Sample 3): " + solution.canAttendMeetings(intervals3));
+
+        // Sample 4: Single meeting
+        int[][] intervals4 = {{10, 20}};
+        System.out.println("Can attend all meetings? (Sample 4): " + solution.canAttendMeetings(intervals4));
+    }
 }
