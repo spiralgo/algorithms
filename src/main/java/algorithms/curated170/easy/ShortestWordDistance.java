8ec79d5f578f24b2da7397c1dff1a77171b868f6
altay.hunoglu <altay.hunoglu@solvians.com>
2024-02-08 00:03:50 +0100

    Adds samples to test it with main method.

--- src/main/java/algorithms/curated170/easy/ShortestWordDistance.java
+++ src/main/java/algorithms/curated170/easy/ShortestWordDistance.java
@@ -17,5 +17,30 @@ public class ShortestWordDistance {
         }
         return minDist;
     }
+    public static void main(String[] args) {
+        ShortestWordDistance solution = new ShortestWordDistance();
 
+        // Test sample 1
+        String[] words1 = {"practice", "makes", "perfect", "coding", "makes"};
+        String word1Sample1 = "coding";
+        String word2Sample1 = "practice";
+        System.out.println("Shortest distance between 'coding' and 'practice': " + solution.shortestDistance(words1, word1Sample1, word2Sample1));
+
+        // Test sample 2
+        String word1Sample2 = "makes";
+        String word2Sample2 = "coding";
+        System.out.println("Shortest distance between 'makes' and 'coding': " + solution.shortestDistance(words1, word1Sample2, word2Sample2));
+
+        // Test sample 3
+        String[] words2 = {"a", "c", "b", "a"};
+        String word1Sample3 = "a";
+        String word2Sample3 = "b";
+        System.out.println("Shortest distance between 'a' and 'b': " + solution.shortestDistance(words2, word1Sample3, word2Sample3));
+
+        // Test sample 4
+        String[] words3 = {"a", "a", "b", "b"};
+        String word1Sample4 = "a";
+        String word2Sample4 = "b";
+        System.out.println("Shortest distance between 'a' and 'b': " + solution.shortestDistance(words3, word1Sample4, word2Sample4));
+    }
 }
